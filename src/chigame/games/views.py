8976ecb0a236eb7ac28f0c5f631e2e23b9f94566
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-10-29 17:41:32 -0500

    New views and templates

--- src/chigame/games/views.py
+++ src/chigame/games/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView
 
 from .models import Game
 
@@ -7,3 +8,17 @@ class GameListView(ListView):
     model = Game
     queryset = Game.objects.all()
     template_name = "games/game_list.html"
+
+
+class GameCreateView(CreateView):
+    model = Game
+    fields = ["name", "description", "min_players", "max_players"]
+    template_name = "games/game_form.html"
+    success_url = reverse_lazy("game-list")
+
+
+class GameEditView(UpdateView):
+    model = Game
+    fields = ["name", "description", "min_players", "max_players"]
+    template_name = "games/game_form.html"
+    success_url = reverse_lazy("game-list")
