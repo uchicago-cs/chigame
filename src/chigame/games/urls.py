7f30a42746b27d97b427e57a1bb6e4178a887880
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-10-29 17:38:20 -0500

    Added new urls for create and edit views. Changed game list view url name to game-list fro consistency

--- src/chigame/games/urls.py
+++ src/chigame/games/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.GameListView.as_view(), name="games-list"),
+    path("", views.GameListView.as_view(), name="game-list"),
+    path("create/", views.GameCreateView.as_view(), name="game-create"),
+    path("<int:pk>/edit", views.GameEditView.as_view(), name="game-edit"),
 ]
