2c3b18f78443f636a82ecfb631fd1c5d77f6a1a4
“Omar <omarelkashef01@gmail.com>
2023-10-29 22:37:09 -0500

    login required to access search result page

--- src/chigame/users/views.py
+++ src/chigame/users/views.py
@@ -1,5 +1,6 @@
 import django.shortcuts as sc
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
@@ -46,6 +47,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 user_redirect_view = UserRedirectView.as_view()
 
 
+@login_required
 def user_search_results(request):
     email = request.GET.get("email")
     users = UserProfile.objects.filter(user__email=email).values()
