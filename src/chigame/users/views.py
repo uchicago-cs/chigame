430acf33df671a6e6b175d435c653f2a4305fab1
root <root@Andrew13-Laptop>
2023-10-30 11:31:55 -0500

    Completed task that creates the admin user page that displays all the users

--- src/chigame/users/views.py
+++ src/chigame/users/views.py
@@ -4,6 +4,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView
+from django.shortcuts import render
 
 User = get_user_model()
 
@@ -41,3 +42,8 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 
 user_redirect_view = UserRedirectView.as_view()
+
+def user_list(request):
+    users = User.objects.all()
+    
+    return render(request, 'user_detail.html', {'users': users})
