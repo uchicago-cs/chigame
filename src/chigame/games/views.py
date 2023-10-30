5de60adc02ca6c9f6dff4426784c2c813be37b56
agustinmoses <121887783+agustinmoses@users.noreply.github.com>
2023-10-30 17:59:36 -0500

    Added the view function for lobby details.

--- src/chigame/games/views.py
+++ src/chigame/games/views.py
@@ -1,9 +1,14 @@
 from django.views.generic import ListView
 
-from .models import Game
+from .models import Game, Lobby
 
 
 class GameListView(ListView):
     model = Game
     queryset = Game.objects.all()
     template_name = "games/game_list.html"
+
+class ViewLobbyDetails(ListView):
+    model = Lobby
+    queryset = Lobby.objects.all()
+    template_name = "games/lobby_details.html"
