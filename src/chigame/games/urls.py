043885a3360dad1bffd522c8eea4b89ce8497f08
Lucien <liuxinyubj@qq.com>
2023-10-30 17:54:31 -0500

    added url and view method for lobby list page

--- src/chigame/games/urls.py
+++ src/chigame/games/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.GameListView.as_view(), name="games-list"),
+    path("", views.lobby_list, name="lobby-list"),
 ]
--- src/chigame/games/views.py
+++ src/chigame/games/views.py
@@ -1,9 +1,15 @@
 from django.views.generic import ListView
+from django.shortcuts import render, redirect, reverse
 
-from .models import Game
+from .models import *
 
 
 class GameListView(ListView):
     model = Game
     queryset = Game.objects.all()
     template_name = "games/game_list.html"
+
+def lobby_list(request):
+    lobbies = Lobby.objects.all()
+    context = {"object_list": lobbies}
+    return render(request, "games/lobby_list.html", context)
