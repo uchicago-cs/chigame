5b8d2e11c8b490139df5583cc6eaa3dae2c89572
“Omar <omarelkashef01@gmail.com>
2023-10-30 17:51:56 -0500

    add and cancel friendship invaitations endpoints

--- src/chigame/users/views.py
+++ src/chigame/users/views.py
@@ -1,13 +1,17 @@
 from django.contrib import messages
+from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView
 
-from .models import UserProfile
+from .models import FriendInvitation, UserProfile
 
 User = get_user_model()
 
@@ -55,3 +59,38 @@ def user_profile_detail_view(request, pk):
     except Exception:
         messages.error(request, "Profile does not exist")
         return redirect(reverse("users:detail", kwargs={"pk": request.user.pk}))
+
+
+@login_required
+def send_friend_invitation(request, pk):
+    sender = User.objects.get(pk=request.user.id)
+    receiver = User.objects.get(pk=pk)
+    if sender.id == receiver.id:
+        messages.error(request, "You can't send friendship invitation to yourself")
+        return redirect(reverse("users:redirect"))
+
+    if sender.id != receiver.id:
+        _, new = FriendInvitation.objects.get_or_create(sender=sender, receiver=receiver)
+    if new:
+        messages.success(request, "Friendship invitation sent successfully.")
+    else:
+        messages.info(request, "Friendship invitation already sent before.")
+    return redirect(reverse("users:redirect"))
+
+
+@login_required
+def cancel_friendship(request, pk):
+    sender = User.objects.get(pk=request.user.id)
+    receiver = User.objects.get(pk=pk)
+    num = None
+    try:
+        friendship = FriendInvitation.objects.get(sender=sender, receiver=receiver)
+        num, _ = friendship.delete()
+    except ObjectDoesNotExist:
+        messages.error(request, "Something went wrong please try again later!")
+    if num:
+        messages.success(request, "Friendship invitation cancelled successfully.")
+    else:
+        messages.error(request, "Something went wrong please try again later!")
+
+    return redirect(reverse("users:redirect"))
