562709aa18140338de9e77165d559de7c938ee16
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-10-30 15:21:40 -0500

    Customized game model on admin interface

--- src/chigame/games/admin.py
+++ src/chigame/games/admin.py
@@ -1,6 +1,30 @@
-from django.apps import apps
 from django.contrib import admin
 
-app_models = apps.get_app_config("games").get_models()
-for model in app_models:
-    admin.site.register(model)
+from .models import Game, Lobby, Match, MatchProposal, Player
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("name", "min_players", "max_players")
+    search_fields = ("name__startswith",)
+
+
+# for future admin page customizations
+@admin.register(Lobby)
+class LobbyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MatchProposal)
+class MatchProposalAdmin(admin.ModelAdmin):
+    pass
