c009dae1c62f3ea184bd9ead548adfa9484c2d20
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-10-30 21:55:34 -0500

    Edit page now redirects to detail page after successful form submission. Added link to edit page from detail page

--- src/chigame/games/views.py
+++ src/chigame/games/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, UpdateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from .models import Game
 
@@ -8,11 +8,13 @@ class GameListView(ListView):
     model = Game
     queryset = Game.objects.all()
     template_name = "games/game_list.html"
-    
+
+
 class GameDetailView(DetailView):
     model = Game
     template_name = "games/game_detail.html"
     context_object_name = "game"
+
 
 class GameCreateView(CreateView):
     model = Game
@@ -20,9 +22,11 @@ class GameCreateView(CreateView):
     template_name = "games/game_form.html"
     success_url = reverse_lazy("game-list")
 
+
 class GameEditView(UpdateView):
     model = Game
     fields = ["name", "description", "min_players", "max_players"]
     template_name = "games/game_form.html"
-    success_url = reverse_lazy("game-list")
-    
+
+    def get_success_url(self):
+        return reverse_lazy("game-detail", kwargs={"pk": self.kwargs["pk"]})
