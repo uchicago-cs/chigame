aa14aafa726b2caee545c61788f3a6ec497738cf
root <root@DESKTOP-EKHT6GC>
2023-11-01 13:12:11 -0500

    Reverted changes to original url

--- src/chigame/games/urls.py
+++ src/chigame/games/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.GameListView.as_view(), name="games-list"),
     path("", views.lobby_list, name="lobby-list"),
-    path("", views.ViewLobbyDetails.as_view(), name ="lobby-details"),
+    path("<int:pk>/", views.GameDetailView.as_view(), name="game-detail"),
 ]
