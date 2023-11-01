9c0be9a10452607d2b6de288c58e29280c0311a2
root <root@Andrew13-Laptop>
2023-10-31 21:43:05 -0500

    updated user_list path urls.py to a new format

--- src/chigame/users/urls.py
+++ src/chigame/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from chigame.users.views import user_detail_view, user_redirect_view, user_update_view, user_list
+from chigame.users.views import user_detail_view, user_redirect_view, user_update_view
+from . import views
 
 app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<int:pk>/", view=user_detail_view, name="detail"),
-    path('user-list/', view=user_list, name='user_list'),
+    path('user-list/', views.user_list, name='user_list'),
 ]
