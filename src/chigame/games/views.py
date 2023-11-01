187d5e266dc6d455b88fae26553ee3e41454d5e1
root <root@DESKTOP-EKHT6GC>
2023-11-01 13:15:15 -0500

    Reverted changes so that we have class instead of a function for detail view

--- src/chigame/games/views.py
+++ src/chigame/games/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render, redirect, reverse
 
 from .models import *
@@ -13,7 +13,7 @@ def lobby_list(request):
     context = {"object_list": lobbies}
     return render(request, "games/lobby_list.html", context)
 
-class ViewLobbyDetails(ListView):
-    model = Lobby
-    queryset = Lobby.objects.all()
-    template_name = "games/lobby_details.html"
+class GameDetailView(DetailView):
+    model = Game
+    template_name = "games/game_detail.html"
+    context_object_name = "game"
