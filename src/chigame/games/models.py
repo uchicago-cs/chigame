9a26c46df72d4f0317f7deaa9bf6f5edc905a07c
MikeChen012345 <2309721935@qq.com>
2023-11-05 21:49:15 -0600

    Updated models.py and change location to the games folder

--- src/chigame/games/models.py
+++ src/chigame/games/models.py
@@ -63,3 +63,97 @@ class MatchProposal(models.Model):
     proposed_time = models.DateTimeField()
     min_players = models.PositiveIntegerField()
     joined = models.ManyToManyField(User, related_name="joined_matches", blank=True)
+
+
+class Tournament(models.Model):
+    """
+    A tournament of a game, between a set of players. Each object represents a
+    single-elimination tournament.
+    """
+
+    name = models.CharField(max_length=255)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    max_players = models.PositiveIntegerField()
+    description = models.TextField()  # not limited to 255 characters
+    rules = models.TextField()  # not limited to 255 characters
+    draw_rules = models.TextField()  # not limited to 255 characters
+    matches = models.ManyToManyField(Match, related_name="matches")
+    winners = models.ManyToManyField(User, related_name="won_tournaments", blank=True)  # allow multiple winners
+    players = models.ManyToManyField(User)
+
+    def get_all_matches(self):
+        return self.matches.all()
+
+    def get_all_winners(self):
+        return self.winners.all()
+
+    def get_all_players(self):
+        return self.players.all()
+
+    def __str__(self):  # may be changed later
+        return (
+            "Tournament "
+            + self.name
+            + ": "
+            + self.game.name
+            + " from "
+            + self.start_date.strftime("%m/%d/%Y")
+            + " to "
+            + self.end_date.strftime("%m/%d/%Y")
+        )
+
+
+class Notification(models.Model):
+    """
+    A notification, which can be sent to multiple users.
+    """
+
+    receipients = models.ManyToManyField(User, related_name="notifications")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    # visibility: a smallPositiveIntegerField representing the visibility of
+    # the notification. This may be added later.
+
+    def get_all_receipients(self):
+        return self.receipients.all()
+
+    def __str__(self):  # may be changed later
+        receipients_str = "&".join([str(receipient) for receipient in self.get_all_receipients()])
+        return "Notification sent to: " + receipients_str + ";\n content: " + self.content
+
+
+class Chat(models.Model):
+    """
+    Represents the live chat feature for both players and spectators.
+    It contains a list of messages, which are not mixed with the messages from
+    other Chats.
+    """
+
+    match = models.OneToOneField(Match, on_delete=models.CASCADE)
+    # match: the match in which the chat is taking place.
+
+    # visibility: a smallPositiveIntegerField representing the visibility of
+    # the chat. This may be added later.
+
+    def __str__(self):  # may be changed later
+        return "Chat for match " + str(self.match)
+
+
+class Message(models.Model):
+    """
+    Represents the individual message which populates one Chat entity. The
+    messages from different Chats are not mixed together.
+    """
+
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)  # timestamp of the
+    # moment the message was created.
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)  # Even if the
+    # user is deleted, the message will still exist.
+
+    def __str__(self):  # may be changed later
+        return "Message from " + str(self.sender) + ": " + self.content
