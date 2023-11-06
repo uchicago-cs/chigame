734d40d9cf4721ac029a877f1642167f577ca1a1
Esterello2 <100042294+Esterello2@users.noreply.github.com>
2023-11-06 12:00:54 -0600

    Update views.py
    Added a check for authenticated users only to be able to send GET, POST, and PATCH requests for User info.

--- src/chigame/users/views.py
+++ src/chigame/users/views.py
@@ -4,9 +4,21 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from .serializers import UserSerializer
 
 User = get_user_model()
+
+class UserInfo(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class UserCreateView(generics.CreateAPIView):
     queryset = User.objects.all()
