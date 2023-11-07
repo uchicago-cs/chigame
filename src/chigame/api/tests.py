121c8513390df2b936dc80a330981d0c25a07893
Arnav Brahmasandra <arnavb@uchicago.edu>
2023-11-07 13:05:17 -0600

    added helper function to check equality #50

--- src/chigame/api/tests.py
+++ src/chigame/api/tests.py
@@ -11,6 +11,14 @@ from chigame.games.models import Game
 
 
 class GameTests(APITestCase):
+    def check_equal(self, object, expected: dict):
+        """
+        Helper function to check that the object data matches the expected data.
+        """
+
+        for key in expected:
+            self.assertEqual(getattr(object, key), expected[key])
+
     def test_create_game(self):
         """
         Ensure we can create a new game object.
@@ -22,11 +30,10 @@ class GameTests(APITestCase):
         response = self.client.post(url, data, format="json")
         # Check that the game object was created correctly
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        game = Game.objects.get()
         self.assertEqual(Game.objects.count(), 1)
-        self.assertEqual(Game.objects.get().name, "The Witcher 3")
-        self.assertEqual(Game.objects.get().description, "Geralt of Rivia")
-        self.assertEqual(Game.objects.get().min_players, 1)
-        self.assertEqual(Game.objects.get().max_players, 4)
+        self.check_equal(game, data)
 
     def test_get_game(self):
         """
@@ -46,10 +53,9 @@ class GameTests(APITestCase):
         # Check that the game object was retrieved correctly
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Game.objects.count(), 1)
-        self.assertEqual(Game.objects.get().name, "The Witcher 3")
-        self.assertEqual(Game.objects.get().description, "Geralt of Rivia")
-        self.assertEqual(Game.objects.get().min_players, 1)
-        self.assertEqual(Game.objects.get().max_players, 4)
+
+        game = Game.objects.get()
+        self.check_equal(game, data)
 
     def test_update_game(self):
         """
@@ -70,10 +76,17 @@ class GameTests(APITestCase):
         # Check that the game object was updated correctly
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Game.objects.count(), 1)
-        self.assertEqual(Game.objects.get().name, "The Witcher 3: Wild Hunt")
-        self.assertEqual(Game.objects.get().description, "Geralt of Rivia")
-        self.assertEqual(Game.objects.get().min_players, 1)
-        self.assertEqual(Game.objects.get().max_players, 5)
+
+        game = Game.objects.get()
+        self.check_equal(
+            game,
+            {
+                "name": "The Witcher 3: Wild Hunt",
+                "description": "Geralt of Rivia",
+                "min_players": 1,
+                "max_players": 5,
+            },
+        )
 
     def test_delete_game(self):
         """
@@ -98,11 +111,17 @@ class GameTests(APITestCase):
         """
         Ensure we can get a list of game objects.
         """
-
-        # Create a game object
         url = reverse("api-game-list")
-        data = {"name": "The Witcher 3", "description": "Geralt of Rivia", "min_players": 1, "max_players": 4}
-        response = self.client.post(url, data, format="json")
+
+        # create two game objects
+        data1 = {"name": "The Witcher 3", "description": "Geralt of Rivia", "min_players": 1, "max_players": 4}
+        response = self.client.post(url, data1, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        data2 = {"name": "Fallout 4", "description": "Vault Dweller", "min_players": 1, "max_players": 2}
+        response = self.client.post(url, data2, format="json")
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # Get the game object list
@@ -111,7 +130,7 @@ class GameTests(APITestCase):
 
         # Check that the game object list was retrieved correctly
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            response.data,
-            [{"name": "The Witcher 3", "description": "Geralt of Rivia", "min_players": 1, "max_players": 4}],
-        )
+
+        # test that the game object list contains the two game objects we created
+        self.assertDictContainsSubset(data1, response.data[0])
+        self.assertDictContainsSubset(data2, response.data[1])
