5cdc35f00deb7ddd5f1d8320b2e5b31a183d811f
Arnav Brahmasandra <arnavb@uchicago.edu>
2023-11-07 10:50:12 -0600

    added the primary y_key to game list view #50

--- src/chigame/api/serializers.py
+++ src/chigame/api/serializers.py
@@ -6,4 +6,4 @@ from chigame.games.models import Game
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = ("name", "description", "min_players", "max_players")
+        fields = ("id", "name", "description", "min_players", "max_players")
