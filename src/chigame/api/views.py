50b79e4a25bef4c7679eb541d041126005428eb0
Arnav Brahmasandra <arnavb@uchicago.edu>
2023-11-07 17:04:46 -0600

    na

--- src/chigame/api/views.py
+++ src/chigame/api/views.py
@@ -1,7 +1,9 @@
 # from django.shortcuts import render
 from rest_framework import generics
+
 from chigame.api.serializers import GameSerializer
 from chigame.games.models import Game
+
 
 class GameListView(generics.ListCreateAPIView):
     queryset = Game.objects.all()
