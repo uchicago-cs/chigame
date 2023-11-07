6e5cc456ab840999c71723263ce9667d46e74806
Arnav Brahmasandra <arnavb@uchicago.edu>
2023-11-07 10:58:01 -0600

    delete comments in views.py and changed names urls

--- src/chigame/api/urls.py
+++ src/chigame/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("games/", views.GameListView.as_view(), name="game-list"),
-    path("games/<int:pk>/", views.GameDetailView.as_view(), name="game-detail"),
+    path("games/", views.GameListView.as_view(), name="api-game-list"),
+    path("games/<int:pk>/", views.GameDetailView.as_view(), name="api-game-detail"),
 ]
--- src/chigame/api/views.py
+++ src/chigame/api/views.py
@@ -4,17 +4,12 @@ from rest_framework import generics
 from chigame.api.serializers import GameSerializer
 from chigame.games.models import Game
 
-# Create your views here.
-
-# create /games/ endpoint that returns a list of all games
-
 
 class GameListView(generics.ListCreateAPIView):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
 
 
-# create /games/<int:pk>/ endpoint that returns a single game
 class GameDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
