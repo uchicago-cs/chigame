571e24dc434a190acf5e23a2c7360fab58a7fc97
frowenz <owenfahey@uchicago.edu>
2023-11-07 11:00:06 -0600

    Protoype of games browse view page

--- src/chigame/games/urls.py
+++ src/chigame/games/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.GameListView.as_view(), name="game-list"),
+    path('browse/', views.game_browse, name='game-browse'),
     path("create/", views.GameCreateView.as_view(), name="game-create"),
     path("<int:pk>/edit", views.GameEditView.as_view(), name="game-edit"),
     path("<int:pk>/", views.GameDetailView.as_view(), name="game-detail"),
