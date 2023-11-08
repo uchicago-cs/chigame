bcd0547f09adb9f6332a0a7824fae00c1b055237
jb-01 <joshuabelofsky@gmail.com>
2023-11-08 13:26:50 -0600

    urls added

--- src/chigame/games/urls.py
+++ src/chigame/games/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path("create/", views.GameCreateView.as_view(), name="game-create"),
     path("<int:pk>/edit", views.GameEditView.as_view(), name="game-edit"),
     path("<int:pk>/", views.GameDetailView.as_view(), name="game-detail"),
+    # tournaments
+    path("tournaments/", views.TournamentsListView.as_view(), name="tournament-list"),
+    path("tournaments/<int:pk>/", views.TournamentDetailView.as_view(), name="tournament-detail"),
+    path("tournaments/create/", views.TournamentCreateView.as_view(), name="tournament-create"),
+    path("tournaments/<int:pk>/update/", views.TournamentUpdateView.as_view(), name="tournament-update"),
+    path("tournaments/<int:pk>/delete/", views.TournamentDeleteView.as_view(), name="tournament-delete"),
 ]
