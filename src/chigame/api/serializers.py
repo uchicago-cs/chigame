6b853097cabaddcc63ff524e251a6cc1da2600cb
Arnav Brahmasandra <arnavb@uchicago.edu>
2023-11-08 17:38:18 -0600

    added user profile urls and views #54

--- src/chigame/api/serializers.py
+++ src/chigame/api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
 from chigame.games.models import Game
+from chigame.users.models import UserProfile
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = ("id", "name", "description", "min_players", "max_players")
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ("id", "display_name", "bio", "date_joined")
--- src/chigame/api/views.py
+++ src/chigame/api/views.py
@@ -24,3 +24,17 @@ class UserProfileListView(generics.ListCreateAPIView):
 class UserProfileDetailView(generics.RetrieveAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+
+
+class UserFriendsAPIView(generics.RetrieveAPIView):
+    serializer_class = UserProfileSerializer
+
+    def get_object(self):
+        user_id = self.kwargs["user_id"]
+        user_profile = UserProfile.objects.get(user_id=user_id)
+        return user_profile.friends.all()
+
+    # def get(self, request, *args, **kwargs):
+    #     friends = self.get_object()
+    #     serializer = UserProfileSerializer(friends, many=True)
+    #     return Response(serializer.data)
