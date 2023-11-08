dc8192880f668ad77beb4d346ebb1df498ebcc93
Arnav Brahmasandra <arnavb@uchicago.edu>
2023-11-08 17:59:13 -0600

    added filters.py to query the Game list view #159

---
+++ src/chigame/api/filters.py
@@ -0,0 +1,13 @@
+import django_filters
+
+from chigame.games.models import Game
+
+
+class GameFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr="icontains")
+    min_players = django_filters.NumberFilter(lookup_expr="exact")
+    max_players = django_filters.NumberFilter(lookup_expr="exact")
+
+    class Meta:
+        model = Game
+        fields = ["name", "description", "min_players", "max_players"]
