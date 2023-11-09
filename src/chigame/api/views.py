5735f751e3ce00f900e7844aef6f5bb9213833b3
Arnav Brahmasandra <arnavb@uchicago.edu>
2023-11-08 18:10:02 -0600

    added filtering to  game list view to query #159

--- src/chigame/api/views.py
+++ src/chigame/api/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 
+from chigame.api.filters import GameFilter
 from chigame.api.serializers import GameSerializer
 from chigame.games.models import Game
 
@@ -8,6 +10,8 @@ from chigame.games.models import Game
 class GameListView(generics.ListCreateAPIView):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
+    filter_backends = (DjangoFilterBackend,)  # Enable DjangoFilterBackend
+    filterset_class = GameFilter  # Specify the filter class for this view
 
 
 class GameDetailView(generics.RetrieveUpdateDestroyAPIView):
