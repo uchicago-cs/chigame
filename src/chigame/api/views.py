9060c134ebc22d2792aae08304d3a7536582e3ef
Arnav Brahmasandra <arnavb@uchicago.edu>
2023-11-09 16:21:40 -0600

    changed serializers in views.py #164

--- src/chigame/api/views.py
+++ src/chigame/api/views.py
@@ -1,15 +1,15 @@
 # from django.shortcuts import render
 from rest_framework import generics
 
-from chigame.api.serializers import GameSerializer
+from chigame.api.serializers import GameDetailSerializer, GameListSerializer
 from chigame.games.models import Game
 
 
 class GameListView(generics.ListCreateAPIView):
     queryset = Game.objects.all()
-    serializer_class = GameSerializer
+    serializer_class = GameListSerializer
 
 
 class GameDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Game.objects.all()
-    serializer_class = GameSerializer
+    serializer_class = GameDetailSerializer
