e5ec884d9750f8957939e8c569a6f5dc0bc1dcef
MikeChen012345 <52188262+MikeChen012345@users.noreply.github.com>
2023-11-09 22:20:26 -0600

    Update 0005_initial.py

--- src/chigame/games/migrations/0005_initial.py
+++ src/chigame/games/migrations/0005_initial.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("games", "0003_initial"),
+        ("games", "0004_initial"),
     ]
 
     operations = [
