cd62f2fe648db81ac4c8be3cd502b987bc1c04ba
ishiii-s <94646741+ishiii-s@users.noreply.github.com>
2023-11-12 20:28:14 -0600

    Update urls.py for remove, tournament, and game paths

--- src/chigame/users/urls.py
+++ src/chigame/users/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from chigame.users.views import (
     cancel_friend_invitation,
     send_friend_invitation,
+    remove_friend,
+    invite_to_game,
+    invite_to_tournament,
     user_detail_view,
     user_profile_detail_view,
     user_redirect_view,
@@ -19,5 +22,8 @@ urlpatterns = [
     path("profile/<int:pk>/", view=user_profile_detail_view, name="user-profile"),
     path("add_friend/<int:pk>", view=send_friend_invitation, name="add-friend"),
     path("cancel_friend_invitation/<int:pk>", view=cancel_friend_invitation, name="cancel-friend-invitation"),
+    path("remove_friend/<int:pk>", view=remove_friend, name="remove-friend"),
+    path("invite_to_game/<int:pk>", view=invite_to_game, name="invite-to-game"),
+    path("invite_to_tournament/<int:pk>", view=invite_to_tournament, name="invite-to-tournament"),
     path("user-list/", views.user_list, name="user_list"),
 ]
