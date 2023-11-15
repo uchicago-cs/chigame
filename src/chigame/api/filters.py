d58ad33e4b0f41c68126935fa52d0a384adb0a5e
Arnav Brahmasandra <arnavb@uchicago.edu>
2023-11-14 18:52:55 -0600

    added comment for icontains #160

--- src/chigame/api/filters.py
+++ src/chigame/api/filters.py
@@ -4,6 +4,8 @@ from chigame.games.models import Game
 
 
 class GameFilter(django_filters.FilterSet):
+    # https://www.w3schools.com/django/ref_lookups_icontains.php
+    # icontains: case-insensitive containment test
     name = django_filters.CharFilter(lookup_expr="icontains")
     min_players = django_filters.NumberFilter(lookup_expr="exact")
     max_players = django_filters.NumberFilter(lookup_expr="exact")
