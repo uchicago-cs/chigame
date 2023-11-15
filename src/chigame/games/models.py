0f3231bd6514682ca9573a250d68903b8a61a999
frowenz <owenfahey@uchicago.edu>
2023-11-15 00:29:45 -0600

    Added validators

--- src/chigame/games/models.py
+++ src/chigame/games/models.py
@@ -1,4 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils import timezone
 
@@ -34,12 +36,39 @@ class Game(models.Model):
     min_playtime = models.PositiveIntegerField(null=True, blank=True)
     max_playtime = models.PositiveIntegerField(null=True, blank=True)
 
-    complexity = models.PositiveSmallIntegerField(null=True, blank=True)  # 1-5, 1 being the easiest
+    complexity = models.DecimalField(
+        max_digits=3, decimal_places=2, null=True, validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+
     category = models.ManyToManyField("Category", related_name="games", blank=True)
     mechanics = models.ManyToManyField("Mechanic", related_name="games", blank=True)
 
     # ================ OTHER ================
     BGG_id = models.PositiveIntegerField(null=True, blank=True)  # BoardGameGeek ID
+
+    # ================ VALIDATON ================
+    def clean(self):
+        # Ensures min_players is not greater than max_players
+        if self.min_players and self.max_players and self.min_players > self.max_players:
+            raise ValidationError({"min_players": "min_players cannot be greater than max_players"})
+
+        # Ensures min_playtime is not greater than max_playtime and that expected_playtime is in this range if provided
+        if self.min_playtime is not None and self.max_playtime is not None:
+            if self.expected_playtime is not None:
+                # Ensures that the expected_playtime is within the range of min_playtime and max_playtime
+                if not (self.min_playtime <= self.expected_playtime <= self.max_playtime):
+                    raise ValidationError(
+                        {"expected_playtime": "expected_playtime must be between min_playtime and max_playtime"}
+                    )
+            else:
+                # Ensure min_playtime is not greater than max_playtime in case where expected_playtime is not provided
+                if self.min_playtime > self.max_playtime:
+                    raise ValidationError({"min_playtime": "min_playtime cannot be greater than max_playtime"})
+
+    def save(self, *args, **kwargs):
+        # Calls full_clean to run all validations before saving
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -132,6 +161,17 @@ class Lobby(models.Model):
     max_players = models.PositiveIntegerField()
     time_constraint = models.PositiveIntegerField(default=300)
     lobby_created = models.DateTimeField(default=timezone.now)
+
+    # ================ VALIDATON ================
+    def clean(self):
+        # Ensures min_players is not greater than max_players
+        if self.min_players > self.max_players:
+            raise ValidationError({"min_players": "min_players cannot be greater than max_players"})
+
+    def save(self, *args, **kwargs):
+        # Calls full_clean to run all validations before saving
+        self.full_clean()
+        super().save(*args, **kwargs)
 
 
 class Match(models.Model):
