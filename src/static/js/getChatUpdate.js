9df7f942f55bd0247e8ea65dc17d8df9304c8e2d
Muyan Xie <1756314803@qq.com>
2023-11-16 01:16:49 -0600

    update the js files and frontend logic to update views of messages in real time

--- src/static/js/getChatUpdate.js
+++ src/static/js/getChatUpdate.js
@@ -1,52 +1,74 @@
 // Simulated frontend data, replace with actual implementation
 let currentTokenId = 0; // Assume the frontend starts with token ID 0
 
-// Function to simulate receiving messages from the backend
+const fakeMessage = [
+  [{ tokenId: 1, updateOn: null, content: "Hello, World!" }],
+  [{ tokenId: 2, updateOn: 1, content: "Updated message!" }],
+  [
+    { tokenId: 3, updateOn: 2, content: null }, // Deleted message
+  ],
+  [{ tokenId: 4, updateOn: null, content: "New message!" }],
+  [{ tokenId: 5, updateOn: 4, content: "Updated message!" }],
+];
+
 function getMessages(tokenId) {
-    // Simulated backend response, replace with actual API call
-    // Here, we assume messages are hardcoded for demonstration purposes
-    const messagesFromBackend = [
-        { tokenId: 1, updateOn: null, content: "Hello, World!" },
-        { tokenId: 2, updateOn: 1, content: "Updated message!" },
-        { tokenId: 3, updateOn: 2, content: null }, // Deleted message
-        { tokenId: 4, updateOn: null, content: "New message!" },
-    ];
-
-    // Filter messages based on the provided tokenId
-    const filteredMessages = messagesFromBackend.filter(message => message.tokenId > tokenId);
-
-    // Update the frontend's currentTokenId
-    currentTokenId = Math.max(...messagesFromBackend.map(message => message.tokenId));
-
-    // Process the filtered messages and update the UI
-    updateUI(filteredMessages);
+  try {
+    const message = fakeMessage[tokenId];
+    return message;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 }
 
-// Function to update the UI with messages
-function updateUI(messages) {
+// Function to simulate receiving messages from the backend
+function updateView(tokenId) {
+  // Simulated backend response, replace with actual API call
+  // Here, we assume messages are hardcoded for demonstration purposes
+
+  const messagesFromBackend = getMessages(tokenId);
+  if (messagesFromBackend == null) {
+    console.log("No new messages");
+  } else {
     const messageContainer = document.getElementById("messageContainer");
 
-    // Update or create elements based on the messages
-    messages.forEach(message => {
+    for (let i = 0; i < messagesFromBackend.length; i++) {
+      const message = messagesFromBackend[i];
+      if (message.updateOn != null) {
+        // modify the message in message Container that has the same tokenId as updateOn
+        const messageElement = document.getElementById(message.updateOn);
+        messageElement.textContent = getMessageText(message);
+        messageElement.id = message.tokenId;
+      } else {
+        // create a new message
         const messageElement = document.createElement("div");
         messageElement.textContent = getMessageText(message);
+        messageElement.id = message.tokenId;
         messageContainer.appendChild(messageElement);
-    });
+      }
+    }
+
+    // Update the current token ID
+    console.log(messagesFromBackend[messagesFromBackend.length - 1].tokenId);
+    currentTokenId =
+      messagesFromBackend[messagesFromBackend.length - 1].tokenId;
+  }
 }
 
 // Function to format message text for display
 function getMessageText(message) {
-    if (message.content !== null) {
-        return message.content;
-    } else {
-        return `Message ${message.tokenId} deleted`;
-    }
+  if (message.content !== null) {
+    return message.content;
+  } else {
+    return `Message ${message.tokenId} deleted`;
+  }
 }
 
 // Simulate initial message retrieval
-getMessages(currentTokenId);
+updateView(currentTokenId);
 
 // Simulate periodic message updates (you may use WebSocket or other real-time mechanisms)
 setInterval(() => {
-    getMessages(currentTokenId);
-}, 1000); // Update every 5 seconds, adjust as needed
+  console.log("Updating messages...");
+  updateView(currentTokenId);
+}, 2000); // Update every second, adjust as needed
