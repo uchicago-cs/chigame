9d22169a2f2dfc71d58999e7d1d792b81e3eb06f
Chloe Lee <jiyeongl@uchicago.edu>
2023-11-20 16:32:57 -0600

    added more filters

--- src/chigame/games/filters.py
+++ src/chigame/games/filters.py
@@ -2,8 +2,10 @@ import django_filters
 from .models import Lobby
 
 class LobbyFilter(django_filters.FilterSet):
+    lobby_name = django_filters.CharFilter(field_name='name', lookup_expr='contains', label='Lobby Name')
     game_name = django_filters.CharFilter(field_name='game_id__name', lookup_expr='contains', label='Game Name')
+    max_players = django_filters.NumberFilter(field_name='max_players', lookup_expr='lt', label='Maximum Number of Players less than or equal to')
 
     class Meta:
         model = Lobby
-        fields = ['game_name']
+        fields = ['lobby_name', 'game_name', 'max_players']
