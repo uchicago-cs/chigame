696b302fb4754cc7f9f62c344edba68ba14a6bd2
Chloe Lee <jiyeongl@uchicago.edu>
2023-11-21 16:56:01 -0600

    try on tasks

--- src/chigame/games/tasks.py
+++ src/chigame/games/tasks.py
@@ -5,7 +5,10 @@ from .models import Lobby
 
 @background(schedule=1)
 def decrement_time_constraint(lobby_pk):
-    lobby = Lobby.objects.get(pk=lobby_pk)
-    if lobby.time_constraint > 0:
-        lobby.time_constraint -= 1
-        lobby.save()
+    try:
+        lobby = Lobby.objects.get(pk=lobby_pk)
+        if lobby.time_constraint > 0:
+            lobby.time_constraint -= 1
+            lobby.save()
+    except Exception as e:
+        print(f"Error in task: {e}")
