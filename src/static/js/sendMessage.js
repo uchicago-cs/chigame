c78d8b542d30ea7827ac76b7d839072cb07fd613
Muyan Xie <1756314803@qq.com>
2023-11-25 00:50:45 -0600

    update

---
+++ src/static/js/sendMessage.js
@@ -0,0 +1,7 @@
+function sendMessage(){
+    var message = document.getElementById("message").value;
+    const id = document.getElementById("id").value;
+    console.log(message);
+    console.log(id);
+
+}
