92ca7a69433dc70995e4366e8de4b98bc34e7de6
Muyan Xie <1756314803@qq.com>
2023-11-25 01:23:29 -0600

    update send logic

--- src/static/js/sendMessage.js
+++ src/static/js/sendMessage.js
@@ -1,7 +1,29 @@
 function sendMessage(){
     var message = document.getElementById("message").value;
-    const id = document.getElementById("id").value;
-    console.log(message);
-    console.log(id);
+    const email = document.getElementById("email").value;
+    const tID = document.getElementById("tID").value;
+    const csrfToken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 
+    if (message != "") {
+
+        var messageData ={
+            "sender": email,
+            "content": message,
+            "update_on": null,
+            "tournament": tID
+        }
+
+        fetch("http://127.0.0.1:8000/api/tournaments/chat/", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+            body: JSON.stringify(messageData),
+        })
+
+    }
+
+    console.log(messageData);
+    document.getElementById("message").value = "";
 }
