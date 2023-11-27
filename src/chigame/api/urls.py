8b87b4713bc1a4fbb5c6c618618b03b3a8427ca8
Arnav Brahmasandra <arnavb@uchicago.edu>
2023-11-26 23:15:16 -0600

    modularized the URLs #294

--- src/chigame/api/urls.py
+++ src/chigame/api/urls.py
@@ -1,16 +1,30 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
+game_patterns = [
+    path("", views.GameListView.as_view(), name="api-game-list"),
+    path("<int:pk>/", views.GameDetailView.as_view(), name="api-game-detail"),
+    path("<int:pk>/categories/", views.GameCategoriesAPIView.as_view(), name="api-game-categories"),
+    path("<int:pk>/mechanics/", views.GameMechanicsAPIView.as_view(), name="api-game-mechanics"),
+]
+
+lobby_patterns = [
+    path("", views.LobbyListView.as_view(), name="api-lobby-list"),
+    path("<int:pk>/", views.LobbyDetailView.as_view(), name="api-lobby-detail"),
+]
+
+user_patterns = [
+    path("", views.UserListView.as_view(), name="api-user-list"),
+    path("<int:pk>/", views.UserDetailView.as_view(), name="api-user-detail"),
+    path("<int:pk>/friends/", views.UserFriendsAPIView.as_view(), name="api-user-friends"),
+]
+
 urlpatterns = [
     # GAME API URLS
-    path("games/", views.GameListView.as_view(), name="api-game-list"),
-    path("games/<int:pk>/", views.GameDetailView.as_view(), name="api-game-detail"),
+    path("games/", include(game_patterns)),
     # LOBBY API URLS
-    path("lobbies/", views.LobbyListView.as_view(), name="api-lobby-list"),
-    path("lobbies/<int:pk>/", views.LobbyDetailView.as_view(), name="api-lobby-detail"),
+    path("lobbies/", include(lobby_patterns)),
     # USER API URLS
-    path("users/", views.UserListView.as_view(), name="api-user-list"),
-    path("users/<int:pk>/", views.UserDetailView.as_view(), name="api-user-detail"),
-    path("users/<int:pk>/friends/", views.UserFriendsAPIView.as_view(), name="api-user-friends"),
+    path("users/", include(user_patterns)),
 ]
