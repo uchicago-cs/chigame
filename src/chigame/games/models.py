e1b12561029a7d5d1be5e866884c2c77232542d3
MikeChen012345 <52188262+MikeChen012345@users.noreply.github.com>
2023-11-27 17:51:36 -0600

    Update models.py line 327 to change < into <=

--- src/chigame/games/models.py
+++ src/chigame/games/models.py
@@ -324,7 +324,7 @@ class Tournament(models.Model):
                 players.append(winner)
 
         # check if the number of players is small enough to end the tournament
-        if len(players) < self.num_winner:
+        if len(players) <= self.num_winner:
             self.end_tournament()
             return []  # the tournament is finished
 
