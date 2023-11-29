fc6b88ab58d96a6d04c75f6571e7362ad786bfea
bburgess19 <bburgess19@cs.uchicago.edu>
2023-11-29 15:23:30 -0600

    Add like model and buttons to forum post

---
+++ src/chigame/forums/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Vote(models.Model):
+    class Rating(models.IntegerChoices):
+        LIKE = 1, "Like"
+        DISLIKE = -1, "Dislike"
+
+    rating = models.IntegerField(choices=Rating.choices)
