c904fe5932a3d5cbfdf10a3b9782596991028eba
Will Cravitz <wcravitz@uchicago.edu>
2023-11-29 23:48:47 -0600

    Merge branch 'dev' into game-management/filter

--- src/chigame/games/forms.py
+++ src/chigame/games/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Game
+from .models import Game, Lobby
 
 
 class GameForm(forms.ModelForm):
@@ -18,3 +18,10 @@ class GameForm(forms.ModelForm):
             "name": forms.TextInput,
             "image": forms.Textarea(attrs={"cols": 80, "rows": 1}),
         }
+
+
+class LobbyForm(forms.ModelForm):
+    class Meta:
+        model = Lobby
+        fields = ["name", "game", "game_mod_status", "min_players", "max_players", "time_constraint"]
+        widgets = {"name": forms.TextInput}
