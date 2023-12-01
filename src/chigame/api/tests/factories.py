d5dde2539be431eb84ea219546090efb5f310069
cuyakonwu <cuyakonwu@uchicago.edu>
2023-12-01 11:59:21 -0600

    Fixed broken import

--- src/chigame/api/tests/factories.py
+++ src/chigame/api/tests/factories.py
@@ -1,7 +1,7 @@
 import random
 
 from factory import Faker, LazyAttribute, Sequence, SubFactory, post_generation
-from django import DjangoModelFactory
+from factory.django import DjangoModelFactory
 
 from chigame.games.models import Category, Chat, Game, Lobby, Mechanic, Tournament
 from chigame.users.models import User
