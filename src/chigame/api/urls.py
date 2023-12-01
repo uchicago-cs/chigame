23ae6eab1232c51d61b97fd06767ee6a663217c4
Esterello2 <arellanoe@uchicago.edu>
2023-12-01 15:34:48 -0600

    fixing CI issue in urls.py

--- src/chigame/api/urls.py
+++ src/chigame/api/urls.py
@@ -12,14 +12,9 @@ urlpatterns = [
     path("lobbies/<int:pk>/", views.LobbyDetailView.as_view(), name="api-lobby-detail"),
     path("users/", views.UserListView.as_view(), name="api-user-list"),
     path("users/<int:pk>/", views.UserDetailView.as_view(), name="api-user-detail"),
-
     path("token/", views.CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", views.CustomTokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", views.CustomTokenVerifyView.as_view(), name="token_verify"),
-    path("users/", views.UserListView.as_view(), name="user-list"),
-
     path("users/<int:pk>/friends/", views.UserFriendsAPIView.as_view(), name="api-user-friends"),
-    # CHAT API URLS
     path("tournaments/chat/", views.MessageView.as_view(), name="api-chat-list"),
-
 ]
