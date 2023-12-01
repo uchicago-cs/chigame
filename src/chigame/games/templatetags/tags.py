363b6724c41564336d52c0939275aed18a43b542
Patrick Lin <patricklin@Patricks-MacBook-Pro.local>
2023-12-01 12:50:31 -0600

    Further comments and documentation

--- src/chigame/games/templatetags/tags.py
+++ src/chigame/games/templatetags/tags.py
@@ -3,7 +3,8 @@ from django import template
 from chigame.games.models import Game
 
 # in order to crete new tags, a module-level instance of a template.Library
-# object must be created that custom tags will be registered to
+# object must be created that custom tags will be registered to. More information
+# about creating custom tags here https://docs.djangoproject.com/en/4.2/howto/custom-template-tags/
 register = template.Library()
 
 
