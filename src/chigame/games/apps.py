c54748b8319c6ebd13b907852c9513b3d31c3c32
frowenz <owenfahey@uchicago.edu>
2023-12-03 14:01:45 -0600

    maybe the real merge is the merge we merged along the way

--- src/chigame/games/apps.py
+++ src/chigame/games/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 from django.core.management import call_command
+from django.db import connection
+from django.db.utils import OperationalError
 
 
 class GamesConfig(AppConfig):
@@ -7,5 +9,15 @@ class GamesConfig(AppConfig):
     name = "chigame.games"
 
     def ready(self):
-        # Load the 'mechanics_categories_fixtures.json' fixture
-        call_command("loaddata", "mechanics_categories_fixtures", app_label="games")
+        """
+        The following code installs the mechanics and categories fixtures into the database
+        The point of the try/expect is to make sure the database is created beforehand
+              This is necessary because `python3 manage.py check` is run before the migrations on the CI
+        """
+        try:
+            # Connection is used to make queries
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT 1 FROM games_mechanic LIMIT 1;")
+                call_command("loaddata", "mechanics_categories_fixtures", app_label="games")
+        except OperationalError:
+            pass
