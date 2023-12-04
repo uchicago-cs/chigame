5b920d1dafe73f9e823d5463456539832790145a
bburgess19 <bburgess19@cs.uchicago.edu>
2023-12-03 23:59:51 -0600

    Use ForumForm class to model form data

---
+++ src/chigame/forums/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from machina.apps.forum.models import Forum
+
+class ForumForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', "")
+        super(ForumForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = Forum
+        fields = ["name", "description", "image", "type"]
+        labels = {
+            "image" : "Upload forum image"
+        }
+        widgets = {
+            "description": forms.Textarea(attrs={"rows": 4}),
+        }
+    
--- src/chigame/forums/views.py
+++ src/chigame/forums/views.py
@@ -3,14 +3,13 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse
 from django.views.generic import CreateView
 from machina.apps.forum.models import Forum
+from .forms import ForumForm
 
 
 class ForumCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
-    model = Forum
-
+    form_class = ForumForm
     success_message = "Forum successfully created!"
     template_name = "forum_form.html"
-    fields = ["name", "description", "image", "type"]
 
     def get_success_url(self):
         return reverse("forum:index")
