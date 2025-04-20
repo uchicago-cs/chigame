829c10f798c552901f72223ca2e13eb37a646cbd
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-20 11:54:51 -0500

    Refactor ASGI configuration for live chat demo project

--- src/sandbox/live-chat-demo/chatdemo/chatdemo/asgi.py
+++ src/sandbox/live-chat-demo/chatdemo/chatdemo/asgi.py
@@ -12,6 +12,13 @@ import os
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
+django_asgi_app = get_asgi_application()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatdemo.settings')
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+})
+
+ASGI_APPLICATION = "chatdemo.asgi.application"
 
 application = get_asgi_application()
