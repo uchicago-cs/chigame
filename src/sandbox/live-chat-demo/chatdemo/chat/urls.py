b50b7d6dd11deb593a7996f67b76e512277afecb
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-20 12:07:44 -0500

    Refactor URL configurations and views for chat application, adding authentication checks and organizing routes.

--- src/sandbox/live-chat-demo/chatdemo/chat/urls.py
+++ src/sandbox/live-chat-demo/chatdemo/chat/urls.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
 from django.urls import path, include
+from chat import views as chat_views
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('chat/', include('chat.urls')),
+    path("", chat_views.chatPage, name="chat-page"),
+
+    # login-section
+    path("auth/login/", LoginView.as_view
+         (template_name="chat/LoginPage.html"), name="login-user"),
+    path("auth/logout/", LogoutView.as_view(), name="logout-user"),
 ]
