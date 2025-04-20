907c6343607a0fb9020fcb03ef30c2142b10687a
Abril Rodriguez <abrilrodriguez@Abrils-MacBook-Air.local>
2025-04-20 13:57:48 -0500

    Finished implementation to address issue #425, awaiting review

--- src/chigame/api/views.py
+++ src/chigame/api/views.py
@@ -15,9 +15,10 @@ from chigame.api.serializers import (
     MechanicSerializer,
     MessageFeedSerializer,
     MessageSerializer,
+    ReviewSerializer,
     UserSerializer,
 )
-from chigame.games.models import Game, Lobby, Message, User
+from chigame.games.models import Game, Lobby, Message, Review, User
 from chigame.users.models import Group, UserProfile
 
 
@@ -156,3 +157,19 @@ class MessageFeedView(APIView):
 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class GameReviewListView(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        game_id = self.kwags["pk"]
+        return Review.objects.filter(game__id=game_id)
+
+
+class ReviewCreateView(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        game_id = self.kwargs["pk"]
+        serializer.save(user=self.request.user, game_id=game_id)
