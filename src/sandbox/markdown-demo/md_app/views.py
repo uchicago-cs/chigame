550b22ea859ea13bed894783f34b36aed09bd19a
Luca Dovichi <lucadovichi@gmail.com>
2025-04-22 18:46:29 -0500

    [knowledge-base] Designed basic sandbox UI, fixed list rendering
    Given that our sandbox renders HTML tags, I wanted the UI to
    clearly delineate what is being rendered vs. what is part
    of the sandbox itself.
    
    Completes task 1 of #513

--- src/sandbox/markdown-demo/md_app/views.py
+++ src/sandbox/markdown-demo/md_app/views.py
@@ -1,18 +1,22 @@
 import markdown
 from django.shortcuts import render
 
+MARKDOWN_STRING = """
+# This is a title
+##  And this is a subheader
+Some content goes here
+
+- with
+- a
+- list
+"""
+
 
 def markdown_content_view(request):
     md = markdown.Markdown(extensions=["fenced_code"])
     markdown_content = {
-        "title": "Knowledge Base Example MD",
-        "content": """
-# This is a title
-##  And this is a subheader
-Some content goes here
-- with
-- a
-- list""",
+        "title": "Knowledge Base Markdown Sandbox",
+        "content": MARKDOWN_STRING,
     }
     context = {"markdown_content": markdown_content}
     markdown_content["content"] = md.convert(markdown_content["content"])
