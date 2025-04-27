d716b1d4feda1d8d6881c91ce3d75ed9fddba91d
Yi Zou <yizou@Yis-MacBook-Air.local>
2025-04-27 13:25:51 -0500

    [knowledge-base] addressed pr feedback
    - change likes and favorites from explicit tables to
    manytomany field
    - put GuideStatus inside Guide object
    
    Closes: #514

--- src/chigame/knowledge_base/models.py
+++ src/chigame/knowledge_base/models.py
@@ -7,28 +7,31 @@ from chigame.games.models import Game
 from chigame.users.models import User
 
 
-class GuideStatus(models.IntegerChoices):
-    PENDING = 0, "Pending"
-    ACCEPTED = 1, "Accepted"
-    REJECTED = 2, "Rejected"
-    REQUESTED_CHANGE = 3, "Requested Change"
-
-
 # record the uploaded guides
 # If a guide is requested change and a new ver is uploaded, this will modify
 # the existing Guide object rather than creating a new Guide object.
 class Guide(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE
-    )  # should we delete the guide if the author deletes their account?
+        User, on_delete=models.CASCADE, related_name="authored_guides"
+    )  # assume we delete this guide if author deletes account
     game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
     content = models.TextField()
     recent_upload = models.DateTimeField(auto_now_add=True)
+
+    class GuideStatus(models.IntegerChoices):
+        PENDING = 0, "Pending"
+        ACCEPTED = 1, "Accepted"
+        REJECTED = 2, "Rejected"
+        REQUESTED_CHANGE = 3, "Requested Change"
+
     status = models.IntegerField(
         choices=GuideStatus.choices,
         default=GuideStatus.PENDING,
     )
-    n_likes = models.IntegerField(default=0)
+
+    # record user likes and favorites
+    likes = models.ManyToManyField(User, related_name="liked_guides")
+    favorites = models.ManyToManyField(User, related_name="favorite_guides")
 
     # manually save the guide object, to update the timestamp if and only if
     # the content field is updated (didn't use auto_now = True, because the
@@ -50,32 +53,14 @@ class Guide(models.Model):
 class ReviewFeedback(models.Model):
     reviewer = models.ForeignKey(
         User, on_delete=models.CASCADE
-    )  # should we delete the guide if the reviewer deletes their account?
+    )  # assume we delete this feedback if the reviewer deletes account
     comment = models.TextField(blank=True, null=True)
     guide_id = models.ForeignKey(Guide, on_delete=models.CASCADE)
     status = models.IntegerField(
         choices=[
-            (GuideStatus.ACCEPTED, "Accepted"),
-            (GuideStatus.REJECTED, "Rejected"),
-            (GuideStatus.REQUESTED_CHANGE, "Requested Change"),
+            (Guide.GuideStatus.ACCEPTED, "Accepted"),
+            (Guide.GuideStatus.REJECTED, "Rejected"),
+            (Guide.GuideStatus.REQUESTED_CHANGE, "Requested Change"),
         ]
     )
     timestamp = models.DateTimeField(auto_now_add=True)
-
-
-# track when a user add a guide to their "favorite"
-class Favorite(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    guide_id = models.ForeignKey(Guide, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ["user_id", "guide_id"]
-
-
-# track when a user "likes" a guide
-class Like(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    guide_id = models.ForeignKey(Guide, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ["user_id", "guide_id"]
