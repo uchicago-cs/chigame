60783af37efebc65b4e406496d6c5e01d4a9e4b6
Yi Zou <yizou@Yis-MacBook-Air.local>
2025-04-26 19:19:02 -0500

    [knowledge-base] fix github pr check
    Changes name in knowledge-base/apps.py to fix failing check,
    CI / run-tests (pull_request).
    
    Fixes: PR #546.

--- src/chigame/knowledge_base/apps.py
+++ src/chigame/knowledge_base/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class KnowledgeBaseConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "knowledge_base"
+    name = "chigame.knowledge_base"
