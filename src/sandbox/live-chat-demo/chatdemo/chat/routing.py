b276310be6ad55ce054065f85c0f91532aa5c3a9
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-28 07:36:22 -0500

    Refactor code for consistency and improved readability across multiple files

--- src/sandbox/live-chat-demo/chatdemo/chat/routing.py
+++ src/sandbox/live-chat-demo/chatdemo/chat/routing.py
@@ -1,8 +1,8 @@
-from django.urls import path , include
 from chat.consumers import ChatConsumer
+from django.urls import include, path
 
 # Here, "" is routing to the URL ChatConsumer which
 # will handle the chat functionality.
 websocket_urlpatterns = [
-    path("" , ChatConsumer.as_asgi()) ,
+    path("", ChatConsumer.as_asgi()),
 ]
