64322e05e96feb7613584143598a46b81e64947d
Karim Sigvaldason <ksigvaldason@uchicago.edu>
2025-04-28 21:07:11 -0500

    Fixed linting issue [WIP: #556]

--- src/chigame/games/models.py
+++ src/chigame/games/models.py
@@ -716,6 +716,7 @@ class LiveChatUser(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     live_chat_id = models.ForeignKey(LiveChat, on_delete=models.CASCADE)
 
+
 class GameList(models.Model):
     """
     A collection of games defined by users.
@@ -730,4 +731,3 @@ class GameList(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.created_by})"
-
