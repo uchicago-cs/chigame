a98a5804311c499fc88663ac0d466bfd9794b3a2
Karim Sigvaldason <ksigvaldason@uchicago.edu>
2025-04-28 21:10:24 -0500

    Fixed black lint issue [WIP: #635]

--- src/chigame/games/models.py
+++ src/chigame/games/models.py
@@ -716,6 +716,7 @@ class LiveChatUser(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     live_chat_id = models.ForeignKey(LiveChat, on_delete=models.CASCADE)
 
+
 class GameList(models.Model):
     """
     A collection of games defined by users.
