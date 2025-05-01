c9193dd7b5df0485923633922aad7a19d32e91ba
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-30 22:13:58 -0500

    Set default room name in ChatConsumer to improve connection handling

--- src/chigame/chat/consumers.py
+++ src/chigame/chat/consumers.py
@@ -5,7 +5,8 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        # Use a default room name instead of getting it from URL parameters
+        self.room_name = "default"
         self.roomGroupName = f"group_chat_{self.room_name}"
         await self.channel_layer.group_add(self.roomGroupName, self.channel_name)
         await self.accept()
