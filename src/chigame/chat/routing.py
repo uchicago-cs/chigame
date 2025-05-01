c48c77de8d31f62397df0dfcd781362b72289d70
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-30 22:05:29 -0500

    Reorganize import statement for ChatConsumer in routing module to enhance clarity and maintain consistency.

--- src/chigame/chat/routing.py
+++ src/chigame/chat/routing.py
@@ -1,5 +1,6 @@
-from chigame.chat.consumers import ChatConsumer
 from django.urls import path
+
+from chigame.chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
     path("", ChatConsumer.as_asgi()),
