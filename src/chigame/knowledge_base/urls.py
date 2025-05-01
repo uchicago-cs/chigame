72fd3002d61f7ee7dd498e0909cdbd5d3460080a
Luca Dovichi <lucadovichi@gmail.com>
2025-05-01 10:49:09 -0500

    [knowledge-base] Fix typo in URL comment
    Fixes #654

--- src/chigame/knowledge_base/urls.py
+++ src/chigame/knowledge_base/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import ContributorView, DefaultView, ModeratorView
 
 urlpatterns = [
-    # games
     path("", DefaultView, name="knowledge-base"),
     path("moderation", ModeratorView, name="knowledge-base-moderator"),
     path("contribution", ContributorView, name="knowledge-base-contributor"),
