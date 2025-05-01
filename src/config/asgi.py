3b3bc18eb6a22abedba75f458c3eb342e4feb410
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-30 21:04:15 -0500

    Refactor ASGI application configuration and add ASGI_APPLICATION setting in base settings

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -15,10 +15,8 @@ from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chigame.settings")
-application = ProtocolTypeRouter(
-    {
+application = ProtocolTypeRouter({
         "http": django_asgi_app,
-    }
-)
+})
 
 ASGI_APPLICATION = "chigame.asgi.application"
--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -62,6 +62,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 ROOT_URLCONF = "config.urls"
 # https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
 WSGI_APPLICATION = "config.wsgi.application"
+# https://docs.djangoproject.com/en/dev/ref/settings/#asgi-application
+ASGI_APPLICATION = "config.asgi.application"
 
 # APPS
 # ------------------------------------------------------------------------------
