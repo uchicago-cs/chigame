d16a6478d625c7c356420a4eb6086262ce0fab3a
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-04-30 21:47:08 -0500

    Refactor chat routing import and streamline HTML template structure for improved readability and consistency.

--- src/chigame/chat/routing.py
+++ src/chigame/chat/routing.py
@@ -1,6 +1,5 @@
+from chat.consumers import ChatConsumer
 from django.urls import path
-
-from chigame.chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
     path("", ChatConsumer.as_asgi()),
