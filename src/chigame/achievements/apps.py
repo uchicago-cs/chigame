e73508a27a5382b232a1fefc8fc49c8cceed9f7d
Benjamin Sim <thatguyben@Benjamins-Laptop.local>
2025-05-02 13:46:25 -0500

    fixed trailing white space

--- src/chigame/achievements/apps.py
+++ src/chigame/achievements/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 class AchievementsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'  
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'chigame.achievements'
-    
