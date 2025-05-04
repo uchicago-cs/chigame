1b5cf2058d8f224aa18ec0fa3ea62d878adb1c58
Abril Rodriguez <abrilrodriguez@Abrils-MacBook-Air.local>
2025-05-04 12:25:22 -0500

    [API] Restored helper function get_user() to original form to fix test errors in GitHub.

--- src/chigame/api/views.py
+++ src/chigame/api/views.py
@@ -25,9 +25,8 @@ from chigame.users.models import Group, UserProfile
 # Helper function to get user from slug
 def get_user(lookup_value):
     # If the lookup_value is an integer, use the id field
-    if lookup_value is not None:
-        if lookup_value.isdigit():
-            return get_object_or_404(User, pk=lookup_value)
+    if lookup_value.isdigit():
+        return get_object_or_404(User, pk=lookup_value)
     else:
         # Otherwise, use the slug field
         return get_object_or_404(User, username=lookup_value)
@@ -174,5 +173,5 @@ class ReviewCreateView(generics.CreateAPIView):
     def perform_create(self, serializer):
         user_id = self.request.data.get("user")
         game_id = self.kwargs["pk"]
-        user = get_user(user_id)
+        user = get_object_or_404(User, pk=user_id)
         serializer.save(user=user, game_id=game_id)
