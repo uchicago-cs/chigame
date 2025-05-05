25c0535bb35dcd0bf1307700f7a4e4ff5f348838
harriet-fisher <harrietfisher@uchicago.edu>
2025-05-05 00:20:35 -0500

    [users] Fixed migration conflict

--- src/chigame/users/views.py
+++ src/chigame/users/views.py
@@ -241,7 +241,7 @@ def send_friend_invitation(request, pk):
     if curr_user.id == other_user.id:
         messages.error(request, "You can't send friendship invitation to yourself")
         return redirect(reverse("users:user-profile", kwargs={"pk": request.user.pk}))
-      
+
     # check if the friendship invitation already exists
     invitation, new = FriendInvitation.objects.filter(
         Q(sender=curr_user, receiver=other_user, is_deleted=False)
@@ -468,6 +468,7 @@ def user_inbox_view(request, pk):
     else:
         messages.error(request, "Not your inbox")
         return redirect(reverse("users:user-profile", kwargs={"pk": request.user.pk}))
+
 
 @login_required
 def remove_friend(request, pk):
