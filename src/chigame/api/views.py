db1161b7ca2481fd342547b2e12af1d781c89b29
Abril Rodriguez <abrilrodriguez@Abrils-MacBook-Air.local>
2025-05-05 15:38:17 -0500

    [API] Implementation of Review Detail View for deleting and patching reviews, addressing issue #503

--- src/chigame/api/views.py
+++ src/chigame/api/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, status
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -175,3 +176,21 @@ class ReviewCreateView(generics.CreateAPIView):
         game_id = self.kwargs["pk"]
         user = get_object_or_404(User, pk=user_id)
         serializer.save(user=user, game_id=game_id)
+
+
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_destroy(self, instance):
+        if instance.user != self.request.user:
+            raise PermissionDenied("You do not have permission to delete this review.")
+        instance.delete()
+
+    def perform_update(self, serializer):
+        user_id = self.request.data.get("user")
+        user = get_object_or_404(User, pk=user_id)
+
+        if user != self.request.user:
+            raise PermissionDenied("You do not have permission to edit this review.")
+        serializer.save()
