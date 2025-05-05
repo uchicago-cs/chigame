c17bf3dfb4946569e7e5a7e6d29c2c3c60eef1a7
Yi Zou <yizou@Yis-MacBook-Air.local>
2025-05-04 21:45:17 -0500

    [knowledge-base] WIP: moderator backend
    - update landing moderator view
    - start on thinking about review guide view

--- src/chigame/knowledge_base/views.py
+++ src/chigame/knowledge_base/views.py
@@ -1,6 +1,9 @@
 # Keep model imports for now, as it will be required for WIP features
 # from .models import *
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from .models import Guide
 
 
 def DefaultView(request):
@@ -8,9 +11,16 @@ def DefaultView(request):
     return render(request, "knowledge-base/landing.html", context)
 
 
+@login_required
 def ModeratorView(request):
-    context = {}
+    pending_guides = Guide.objects.filter(status=0)
+    context = {"pendingGuides": pending_guides}
     return render(request, "knowledge-base/moderator.html", context)
+
+
+@login_required
+def ReviewDetail(request, pk):
+    pass  # to be completed, should display the guide info, and the approve/reject/request change button
 
 
 def ContributorView(request):
