8e5652cab7ba673d3da154526fe43fdb120f174a
Md Hossain <hossainmd@Nerdy>
2025-05-05 01:05:20 -0500

    linting fix

--- src/chigame/users/views.py
+++ src/chigame/users/views.py
@@ -184,10 +184,14 @@ def user_profile_detail_view(request, pk):
         is_friend = target_user.friends.filter(pk=request.user.pk).exists()
         if not is_friend:
             curr_user = request.user
-            friendship_request = FriendInvitation.objects.filter(
-                Q(sender=target_user, receiver=curr_user, is_deleted=False)
-                | Q(sender=curr_user, receiver=target_user, is_deleted=False)
-            ).order_by("-timestamp").first()
+            friendship_request = (
+                FriendInvitation.objects.filter(
+                    Q(sender=target_user, receiver=curr_user, is_deleted=False)
+                    | Q(sender=curr_user, receiver=target_user, is_deleted=False)
+                )
+                .order_by("-timestamp")
+                .first()
+            )
 
     # provide frontend profile + friendship status
     context = {"object": profile, "is_friend": is_friend, "friendship_request": friendship_request}
