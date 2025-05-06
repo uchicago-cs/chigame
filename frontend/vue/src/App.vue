73d2b72a53d652e3f092647ec49d97c249e79faa
clairezzhang <109111127+clairezzhang@users.noreply.github.com>
2025-05-06 14:46:12 -0500

    [frontend] Convert navbar logo to button
    Converted the navbar logo to a button that redirects to the homepage
    in the Vue frontend. Also added the font and styling to the text in the
    logo to ensure consistency with other Chigame branding.
    
    Closes: #733

--- frontend/vue/src/App.vue
+++ frontend/vue/src/App.vue
@@ -1,4 +1,20 @@
 <template>
+  <head>
+  <!-- Latest compiled and minified Bootstrap CSS -->
+  <link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css"
+      integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer" />
+  <!-- Bootstrap CSS Icons-->
+  <link rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css" />
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap"
+        rel="stylesheet" />
+  </head>
   <div id="app">
     <NavBar />
     <router-view />
