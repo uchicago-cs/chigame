b2bb7dd3341837f479b8cee2497f3837c967febf
Luca Dovichi <lucadovichi@gmail.com>
2025-05-05 19:49:46 -0500

    [knowledge-base] First pass of cleaning accidental commits
    WIP: #641

--- src/sandbox/markdown-demo/md_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.markdown_content_view, name="markdown_content"),
-    path("minimal/", views.markdown_content_view, {"minimal": True}, name="minimal_content"),
-]
