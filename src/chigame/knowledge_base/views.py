8f82daadc48203e6c1817055b373cec2cb480f56
Yi Zou <yizou@Yis-MacBook-Air.local>
2025-05-07 16:26:57 -0500

    [knowledge-base] update landing page
    - small edits on links on landing page
    
    For: #747

--- src/chigame/knowledge_base/views.py
+++ src/chigame/knowledge_base/views.py
@@ -8,7 +8,12 @@ from .models import Guide
 def DefaultView(request):
     guide_ids = Game.objects.values_list("published_guide_id", flat=True).distinct()
     guides = Guide.objects.filter(id__in=guide_ids)
-    context = {"guides": guides}
+
+    has_guides = False  # if the user hasn't uploaded any guide,
+    # "manage your guide" button will not show up
+    if request.user.is_authenticated:
+        has_guides = Guide.objects.filter(author=request.user).exists()
+    context = {"guides": guides, "has_guides": has_guides}
     return render(request, "knowledge-base/landing.html", context)
 
 
