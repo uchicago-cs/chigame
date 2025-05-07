8d7f5a4ae8f80709f8c3e4868d80cbd5e76b7a4d
Yi Zou <yizou@Yis-MacBook-Air.local>
2025-05-07 16:49:54 -0500

    [knowledge-base] WIP: landing page backend
    - change it to generic view
    
    For: #747

--- src/chigame/knowledge_base/urls.py
+++ src/chigame/knowledge_base/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import ContributorView, DefaultView, ModeratorView
 
 urlpatterns = [
-    path("", DefaultView, name="knowledge-base"),
+    path("", DefaultView.as_view(), name="knowledge-base"),
     path("moderation", ModeratorView, name="knowledge-base-moderator"),
     path("contribution", ContributorView, name="knowledge-base-contributor"),
 ]
--- src/chigame/knowledge_base/views.py
+++ src/chigame/knowledge_base/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from chigame.games.models import Game
 
 from .models import Guide
 
 
-def DefaultView(request):
-    guide_ids = Game.objects.values_list("published_guide_id", flat=True).distinct()
-    guides = Guide.objects.filter(id__in=guide_ids)
+class DefaultView(ListView):
+    model = Guide
+    template_name = "knowledge-base/landing.html"
+    context_object_name = "guides"
 
-    has_guides = False  # if the user hasn't uploaded any guide,
-    # "manage your guide" button will not show up
-    if request.user.is_authenticated:
-        has_guides = Guide.objects.filter(author=request.user).exists()
-    context = {"guides": guides, "has_guides": has_guides}
-    return render(request, "knowledge-base/landing.html", context)
+    def get_queryset(self):
+        guide_ids = Game.objects.values_list("published_guide_id", flat=True).distinct()
+        return Guide.objects.filter(id__in=guide_ids)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["has_guides"] = (
+            self.request.user.is_authenticated and Guide.objects.filter(author=self.request.user).exists()
+        )  # if the user hasn't uploaded any guide, "manage your guide" button will not show up
+        return context
 
 
 def ModeratorView(request):
