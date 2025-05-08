b86394ec38396aa70ce18c1f0f1a68ec2f96707f
David Makoyo <makoyo.david@me.com>
2025-05-07 23:11:11 -0500

    [Knowledge Base] Created an HTML page for the contributors to managed uploaded guides (#754)
    * [knowledge-base] WIP: began work on contributor page frontend
    
    * [knowledge-base] WIP: added mock guide entries to contributor view table
    
    * [knowledge-base] #687: refactor contributor UI to use CSS classes instead of inline styles
    
    ---------
    
    Co-authored-by: mrollinswaterman <mrollinswaterman@uchicago.edu>
    Co-authored-by: mrollinswaterman <146126429+mrollinswaterman@users.noreply.github.com>
    Co-authored-by: jacqsmei <54288733+jacqsmei@users.noreply.github.com>

--- src/chigame/knowledge_base/views.py
+++ src/chigame/knowledge_base/views.py
@@ -1,11 +1,18 @@
 # Keep model imports for now, as it will be required for WIP features
-# from .models import *
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Guide
 
 
 def DefaultView(request):
     context = {}
     return render(request, "knowledge-base/landing.html", context)
+
+
+def GuideDetailView(request, pk):
+    guide = get_object_or_404(Guide, pk=pk)
+    context = {"guide": guide}
+    return render(request, "knowledge-base/guide_detail.html", context)
 
 
 def ModeratorView(request):
