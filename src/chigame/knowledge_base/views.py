28b12e0619ad8618313232655e127a98cba04c1d
davidmakoyo <makoyo.david@me.com>
2025-05-08 10:06:53 -0500

    [knowledge-base] move frontend changes to frontend branch WIP #749

--- src/chigame/knowledge_base/views.py
+++ src/chigame/knowledge_base/views.py
@@ -1,7 +1,7 @@
 # Keep model imports for now, as it will be required for WIP features
 # from .models import *
-from django.contrib.auth.decorators import login_required
-from django.core.exceptions import PermissionDenied
+# from django.contrib.auth.decorators import login_required
+# from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 
 from .models import Guide
@@ -12,10 +12,7 @@ def DefaultView(request):
     return render(request, "knowledge-base/landing.html", context)
 
 
-@login_required
 def ModeratorGuidesPending(request):
-    if not request.user.moderator:
-        raise PermissionDenied()
     pending_guides = Guide.objects.filter(status=0)
     context = {"pendingGuides": pending_guides}
     return render(request, "knowledge-base/moderator_pending_guides.html", context)
