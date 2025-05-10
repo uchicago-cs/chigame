6c80ae9a9af9469c461acff3d1c62eb66292ef94
yizou275 <155184432+yizou275@users.noreply.github.com>
2025-05-10 14:12:40 -0500

    [knowledge-base] Add Viewer Landing Page Backend (#778)
    * [knowledge-base] WIP: landing page view
    
    Sync my code on landing page backend. Can display the published guides
    now. Not completed "contribute" or "manage guide" component yet.
    Also lack filtering or searching feature, which can be done here or in
    separate issues.
    
    * [knowledge-base] update landing page
    
    - small edits on links on landing page
    
    For: #747
    
    * [knowledge-base] WIP: landing page backend
    
    - change it to generic view
    
    For: #747
    
    * [knowledge-base] landing page backend
    
    - update on filtering, searching, and sorting
    
    For: #747
    
    * [knowledge-base] landing page backend edit
    
    - add comment to clarify my code, last small edit
    
    * [knowledge-base] change landing html
    
    - hide "Note: ..." for logged in users
    - fix the visual issue with filtering reset
    
    For: #747
    
    ---------
    
    Co-authored-by: Yi Zou <yizou@Yis-MacBook-Air.local>
    Co-authored-by: jacqsmei <54288733+jacqsmei@users.noreply.github.com>

--- src/chigame/knowledge_base/views.py
+++ src/chigame/knowledge_base/views.py
@@ -1,12 +1,63 @@
-# Keep model imports for now, as it will be required for WIP features
+from django.db.models import CharField, F, Q, Value
+from django.db.models.functions import Concat
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
+
+from chigame.games.models import Category, Game
 
 from .models import Guide
 
 
-def DefaultView(request):
-    context = {}
-    return render(request, "knowledge-base/landing.html", context)
+class DefaultView(ListView):
+    model = Guide
+    template_name = "knowledge-base/landing.html"
+    context_object_name = "guides"
+
+    def get_queryset(self):
+        guide_ids = Game.objects.values_list("published_guide_id", flat=True).distinct()
+        queryset = Guide.objects.filter(id__in=guide_ids)
+
+        query = self.request.GET.get("q")
+
+        if query:
+            # the query is originally on the game name of Guide object, and will
+            # fail for queries like "Guide for [game name]"
+            # here add queryset.annotate to make query for "Guide for..." works
+            queryset = queryset.annotate(
+                guide_title=Concat(Value("Guide for "), F("game_id__name"), output_field=CharField())
+            ).filter(Q(guide_title__icontains=query) | Q(content__icontains=query))
+
+        # for filtering; only support single-choice filtering for now
+        category = self.request.GET.get("category")
+        if category:
+            categorymatch = Category.objects.get(name=category)
+            queryset = queryset.filter(game__categories=categorymatch)
+
+        # for sorting
+        sort = self.request.GET.get("sort")
+        if sort == "az":
+            queryset = queryset.order_by("game__name")
+        elif sort == "za":
+            queryset = queryset.order_by("-game__name")
+        elif sort == "old":
+            queryset = queryset.order_by("recent_upload")
+        else:  # default: newest first
+            queryset = queryset.order_by("-recent_upload")
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # make sure "manage my guide" only shows up when the user uploads
+        # a guide previously
+        context["has_guides"] = (
+            self.request.user.is_authenticated and Guide.objects.filter(author=self.request.user).exists()
+        )
+        # to facilitate filtering
+        context["categories"] = Category.objects.filter(
+            id__in=Game.objects.values_list("categories", flat=True).distinct()
+        )
+        return context
 
 
 def GuideDetailView(request, pk):
