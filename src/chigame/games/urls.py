9207bbde4013db7be295911db3c354f2f3f0062a
carriesunluo05 <carrieluo05@uchicago.edu>
2025-05-10 15:36:47 -0500

    [Tournament] updated code for UD models

--- src/chigame/games/urls.py
+++ src/chigame/games/urls.py
@@ -44,4 +44,10 @@ urlpatterns = [
     path("tournaments/archived/", views.TournamentArchivedListView.as_view(), name="tournament-archived"),
     # placeholder game
     path("lobby/<int:pk>/coinflip", views.coin_flip_game, name="placeholder-game"),
+    # tournament feedback
+    path("tournaments/<int:tournament_id>/feedback/", views.tournament_feedback_list, name="tournament-feedback-list"),
+    path("tournaments/<int:tournament_id>/feedback/submit/", views.submit_feedback, name="submit-feedback"),
+    path("feedback/update/<int:feedback_id>/", views.update_feedback_view, name="update-feedback"),
+    path("feedback/delete/<int:feedback_id>/", views.delete_feedback_view, name="delete-feedback"),
+    path("feedback/my-feedback/", views.user_feedback_list, name="user-feedback-list"),
 ]
--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="v2ABJ0XFjbJ12cvh10dhAQIUdDYjHpfcIy2kqIDawXeCVxKhBvqTRSM4Pdo9Uv8G",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "testserver"]
 
 # CACHES
 # ------------------------------------------------------------------------------
