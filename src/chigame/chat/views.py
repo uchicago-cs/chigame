0766b17b64619b13d5f2f03c8f630229021aac34
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-05-10 17:47:41 -0500

    Had made a pretty large mistake, was not using the base base but rather had copied it over

--- src/chigame/chat/views.py
+++ src/chigame/chat/views.py
@@ -5,6 +5,6 @@ from .models import LiveChat, LiveChatMessage
 
 def chat(request, chat_id):
     chat = get_object_or_404(LiveChat, id=chat_id)
-    messages = LiveChatMessage.objects.filter(live_chat=chat).order_by('created_at')
+    messages = LiveChatMessage.objects.filter(live_chat=chat).order_by("created_at")
 
     return render(request, "chat/index.html", {"chat": chat, "messages": messages})
