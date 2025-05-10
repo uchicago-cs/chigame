2e12dc0611c424b27b81e9aea5aabd677f3f30eb
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-05-10 17:19:05 -0500

    [chat] Refactor chat view and templates to streamline message handling
    Updated the chat view to simplify message retrieval and removed unnecessary code for clearing Django messages. Modified the chat template to extend a new base template and eliminated JavaScript for hiding alert messages, enhancing code clarity and maintainability.
    
    Closes: #XXX

--- src/chigame/chat/views.py
+++ src/chigame/chat/views.py
@@ -5,11 +5,6 @@ from .models import LiveChat, LiveChatMessage
 
 def chat(request, chat_id):
     chat = get_object_or_404(LiveChat, id=chat_id)
-    messages_list = LiveChatMessage.objects.filter(live_chat=chat)
+    messages = LiveChatMessage.objects.filter(live_chat=chat)
     
-    # Clear any Django messages
-    storage = messages.get_messages(request)
-    for message in storage:
-        pass  # Iterating clears the messages
-    
-    return render(request, "chat/index.html", {"chat": chat, "messages": messages_list})
+    return render(request, "chat/index.html", {"chat": chat, "messages": messages})
