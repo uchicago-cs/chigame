c2efeee6ed7a8ddaf5adb7c7009dc32e40f8cb00
Yi Zou <yizou@Yis-MacBook-Air.local>
2025-05-11 11:57:51 -0500

    [knowledge-base] WIP: contributor backend

--- src/chigame/knowledge_base/urls.py
+++ src/chigame/knowledge_base/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import ContributorView, DefaultView, ModeratorView
+from .views import ContributorManageGuide, DefaultView, DownloadGuide, ModeratorView
 
 urlpatterns = [
     path("", DefaultView, name="knowledge-base"),
     path("moderation", ModeratorView, name="knowledge-base-moderator"),
-    path("contribution", ContributorView, name="knowledge-base-contributor"),
+    path("manage-my-guides", ContributorManageGuide, name="knowledge-base-contributor"),
+    path("guides/<int:pk>/download-md/", DownloadGuide, name="download-guide"),
 ]
 
 # Eventually, we should add '!<slug:username>' the contribution path so we can
--- src/chigame/knowledge_base/views.py
+++ src/chigame/knowledge_base/views.py
@@ -1,4 +1,6 @@
 # Keep model imports for now, as it will be required for WIP features
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Guide
@@ -20,6 +22,19 @@ def ModeratorView(request):
     return render(request, "knowledge-base/moderator.html", context)
 
 
-def ContributorView(request):
-    context = {}
-    return render(request, "knowledge-base/contributor.html", context)
+@login_required
+def ContributorManageGuide(request):
+    guides = request.user.authored_guides.all()
+    context = {"guides": guides}
+    return render(request, "knowledge-base/contributor_manage_guide.html", context)
+
+
+@login_required
+def DownloadGuide(request, pk):
+    guide = get_object_or_404(Guide, pk=pk)
+    content = guide.content  # Assuming this is already Markdown or close to it
+
+    filename = f"guide_{guide.pk}.md"
+    response = HttpResponse(content, content_type="text/markdown")
+    response["Content-Disposition"] = f'attachment; filename="{filename}"'
+    return response
