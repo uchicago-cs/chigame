42cc5843c59c097b3bdae409c6d5ab3ab90f3192
zaynacheema <146792920+zaynacheema@users.noreply.github.com>
2025-05-11 14:22:12 -0500

    Update urls.py

--- src/chigame/games/urls.py
+++ src/chigame/games/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-from .views import LobbyCreateView, UploadFileView
+from .views import LobbyCreateView, UploadFileView, InteractiveFictionView
 
 urlpatterns = [
     # lobbies
