bc53ce246479c2c482b62907f8ee48beb4cbd6c2
Nathaniel <nneidlinger@uchicago.edu>
2025-05-10 20:33:12 -0500

    Fixed Colors to be 6 digits

--- src/sandbox/checkers-prototype/game.js
+++ src/sandbox/checkers-prototype/game.js
@@ -28,8 +28,8 @@ const COLORS = {
   black: 0x000000,
   red: 0xff0000,
   white: 0xffffff,
-  colorblind_blue: 0x1e88e5ff,
-  colorblind_orange: 0xffc107ff,
+  colorblind_blue: 0x1e88e5,
+  colorblind_orange: 0xffc107,
 };
 let pieces = [];
 let selectedPiece = null;
