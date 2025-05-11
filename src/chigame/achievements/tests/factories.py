d6210e578f47dca72139bd6ba004ab88f0fa1c97
Benjamin Sim <thatguyben@Benjamins-Laptop.local>
2025-05-11 14:10:10 -0500

    [achievements] fixed format of factories file, ran the CI locally and made correct changes

--- src/chigame/achievements/tests/factories.py
+++ src/chigame/achievements/tests/factories.py
@@ -1,27 +1,31 @@
 import factory
 from factory.django import DjangoModelFactory
+
+from chigame.achievements.models import Achievement, User, UserAchievement
 from chigame.api.tests.factories import GameFactory
-from chigame.achievements.models import User, Achievement, UserAchievement
+
 
 # General user factory
 class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
 
-    username = factory.Faker('user_name')
-    email = factory.Faker('email')
-    password = factory.PostGenerationMethodCall('set_password', 'password')
+    username = factory.Faker("user_name")
+    email = factory.Faker("email")
+    password = factory.PostGenerationMethodCall("set_password", "password")
+
 
 class AchievementFactory(DjangoModelFactory):
     class Meta:
         model = Achievement
 
-    name = factory.Faker('word')
-    description = factory.Faker('text')
-    spoiler = factory.Faker('boolean')
-    rarity = factory.Faker('random_int', min=1, max=4)
+    name = factory.Faker("word")
+    description = factory.Faker("text")
+    spoiler = factory.Faker("boolean")
+    rarity = factory.Faker("random_int", min=1, max=4)
     game = factory.SubFactory(GameFactory)
-    threshold = factory.Faker('random_number', digits=2)  # Random number for threshold
+    threshold = factory.Faker("random_number", digits=2)  # Random number for threshold
+
 
 class UserAchievementFactory(DjangoModelFactory):
     class Meta:
@@ -29,6 +33,6 @@ class UserAchievementFactory(DjangoModelFactory):
 
     user = factory.SubFactory(UserFactory)
     achievement = factory.SubFactory(AchievementFactory)
-    pinned = factory.Faker('boolean')
-    date_earned = factory.Faker('date_time_this_year')
-    progress = factory.Faker('random_number', digits=2)  # Random number for progress
+    pinned = factory.Faker("boolean")
+    date_earned = factory.Faker("date_time_this_year")
+    progress = factory.Faker("random_number", digits=2)  # Random number for progress
