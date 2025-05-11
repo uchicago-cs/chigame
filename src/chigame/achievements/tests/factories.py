a6dfdc646884ebc12f7e77bdee16c755c5b1a2ee
Benjamin Sim <thatguyben@Benjamins-Laptop.local>
2025-05-11 14:01:27 -0500

    [achievements] Created achievement model factories for testing

---
+++ src/chigame/achievements/tests/factories.py
@@ -0,0 +1,34 @@
+import factory
+from factory.django import DjangoModelFactory
+from PROJECT_DIR.chigame.src.chigame.api.tests.factories import GameFactory
+from chigame.achievements.models import *
+
+### General user factory
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
+    password = factory.PostGenerationMethodCall('set_password', 'password')
+
+class AchievementFactory(DjangoModelFactory):
+    class Meta:
+        model = Achievement
+
+    name = factory.Faker('word')
+    description = factory.Faker('text')
+    spoiler = factory.Faker('boolean')
+    rarity = factory.Faker('random_int', min=1, max=4)
+    game = factory.SubFactory(GameFactory)
+    threshold = factory.Faker('random_number', digits=2)  # Random number for threshold
+
+class UserAchievementFactory(DjangoModelFactory):
+    class Meta:
+        model = UserAchievement
+
+    user = factory.SubFactory(UserFactory)
+    achievement = factory.SubFactory(AchievementFactory)
+    pinned = factory.Faker('boolean')
+    date_earned = factory.Faker('date_time_this_year')
+    progress = factory.Faker('random_number', digits=2)  # Random number for progress
