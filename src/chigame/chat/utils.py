1ff728c2939f1e66cac79cf41c52ceb2bb637eaa
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-05-11 17:37:27 -0500

    Added profanity filtering class and utils.py

---
+++ src/chigame/chat/utils.py
@@ -0,0 +1,41 @@
+"""
+Utilities for the chat app. Includes profanity filtering.
+"""
+
+import re
+
+class ProfanityFilter:
+    """
+    A simple class that allows for filtering of profanity and censoring of messages.
+    """
+
+    # a list of profane words
+    # this list should be updated with new profane words as they are discovered
+    profane_words = [
+        "uchicago sucks",
+        "uchicago is a bad school",
+        "uchicago is a bad university",
+        "uchicago is a bad college",
+        "uchicago is a bad school",
+    ]
+
+    def __init__(self):
+        """
+        Initialize the profanity filter.
+        """
+        escaped_words = [re.escape(word) for word in self.profanity_list] # create the regex patterns
+        self.pattern = re.compile(r"\b(" + "|".join(escaped_words) + r")\b", re.IGNORECASE)
+
+    def contains_profanity(self, message):
+        """
+        Check if the message contains any profane words.
+
+        Args:
+            message (str): The message to check for profane words.
+
+        Returns:
+            bool: True if the message contains any profane words, False otherwise.
+        """
+        return bool(self.pattern.search(message))
+            
+    
