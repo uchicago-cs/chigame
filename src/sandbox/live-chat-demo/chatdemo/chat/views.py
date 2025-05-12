465b416d9a6a3cbe37d663f22711fc7715697825
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-05-12 15:47:31 -0500

    Add docstrings to ChatConsumer methods and update models.py with explanation for demo app. Enhance chatPage view with comments for future modifications.

--- src/sandbox/live-chat-demo/chatdemo/chat/views.py
+++ src/sandbox/live-chat-demo/chatdemo/chat/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import redirect, render
 
 
-def chatPage(request, *args, **kwargs):
+def chatPage(request):
+    """
+    This is the main page for the chat. This is what you would edit if you would like to add a login system, etc.
+
+    Returns:
+        HttpResponse: The response object.
+    """
     if not request.user.is_authenticated:
         return redirect("login-user")
-    context = {}
-    return render(request, "chat/chatPage.html", context)
+    
+    # if you wanted to implement chats loaded from db (stored) you would implement some sort of arg here
+    # you would also add, like in the chat app, messages fetched from that chat
+
+    return render(request, "chat/chatPage.html", {})
