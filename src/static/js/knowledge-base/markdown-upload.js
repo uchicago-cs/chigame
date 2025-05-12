0cf014aabe2992d69f77c6e7f38380716abc3466
Luca Dovichi <lucadovichi@gmail.com>
2025-05-11 22:20:59 -0500

    [knowledge-base] remove unnecessary js listeners

--- src/static/js/knowledge-base/markdown-upload.js
+++ src/static/js/knowledge-base/markdown-upload.js
@@ -2,10 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const fileInput = document.getElementById("id_file");
   const fileUploadLabel = document.querySelector(".file-upload");
 
-  // Prevent default behavior for drag events
-  fileUploadLabel.addEventListener("dragover", (e) => e.preventDefault());
-  fileUploadLabel.addEventListener("dragenter", (e) => e.preventDefault());
-
   // Handle file drop
   fileUploadLabel.addEventListener("drop", (e) => {
     e.preventDefault();
