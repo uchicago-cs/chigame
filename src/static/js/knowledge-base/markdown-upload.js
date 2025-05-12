49b0f24515c35efb82ae4c8b550cf86997d80dcd
Luca Dovichi <lucadovichi@gmail.com>
2025-05-12 10:56:27 -0500

    [knowledge-base] minor styling enhancements
    WIP: #830

--- src/static/js/knowledge-base/markdown-upload.js
+++ src/static/js/knowledge-base/markdown-upload.js
@@ -14,13 +14,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const files = e.dataTransfer.files;
     if (files.length > 0) {
       fileInput.files = files; // Assign dropped files to the file input
-      fileNameArea.textContent = files[0].name; // Update label text with file name
+      fileNameArea.textContent = `Selected "${files[0].name}"`; // Update label text with file name
       console.log("here");
     }
   });
 
   // handle file uploaded via click
   fileInput.addEventListener("change", (e) => {
-    fileNameArea.textContent = e.target.files[0].name;
+    fileNameArea.textContent = `Selected ${e.target.files[0].name}`;
   });
 });
