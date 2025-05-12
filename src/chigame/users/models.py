c0d50cb7a6609490e31bcf31d685ea2fa6501f23
harriet-fisher <harrietfisher@uchicago.edu>
2025-05-11 22:10:50 -0400

    [Users] Removed uniqueness constraint on friendInvitations for issue #559

--- src/chigame/users/models.py
+++ src/chigame/users/models.py
@@ -108,9 +108,6 @@ class FriendInvitation(models.Model):
     objects = FriendInvitationManager()
     is_deleted = models.BooleanField(default=False)
 
-    class Meta:
-        unique_together = ("sender", "receiver")
-
     def accept_invitation(self):
         """
         Accept a friend invitation.
