cdee80563b7caf627898f8bab1f09b998e7b6b37
jonathanbacon22 <jonathanbacon@uchicago.edu>
2025-05-11 20:56:36 -0500

    [User Profiles + Friending] linter for PR #774

--- src/chigame/users/views.py
+++ src/chigame/users/views.py
@@ -677,7 +677,6 @@ def view_bookmarked_notifications(request, pk):
         return redirect(reverse("users:user-profile", kwargs={"pk": request.user.pk}))
 
 
-
 @login_required
 def upload_profile_photo(request):
     if request.method == "POST" and request.FILES.get("photo"):
@@ -686,6 +685,7 @@ def upload_profile_photo(request):
         profile.save()
         messages.success(request, "Profile photo updated.")
     return redirect(reverse("users:user-profile", kwargs={"pk": request.user.pk}))
+
 
 @csrf_protect
 @require_POST
