7de330c89ac983aae2789478c32adaaa70b2df91
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-05-12 23:04:41 -0500

    Add user to public chat: include request user in chat users if chat is public

--- src/chigame/chat/views.py
+++ src/chigame/chat/views.py
@@ -7,6 +7,10 @@ def chat(request, chat_id):
     chat = get_object_or_404(LiveChat, id=chat_id)
     messages = LiveChatMessage.objects.filter(live_chat=chat).order_by("sent_at")
 
+    # if the chat is public, add the request user to the chat
+    if chat.public:
+        chat.users.add(request.user)
+
     return render(request, "chat/index.html", {"chat": chat, "messages": messages})
 
 def live_chat_list(request):
