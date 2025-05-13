3e22e7dcd01d08576d84b106a5ac763eb98c307e
Quincy Leung <quincyleung@uchicago.edu>
2025-05-13 17:58:00 -0500

    [live-chat]Added authentication check and private chat filtering

--- src/chigame/chat/views.py
+++ src/chigame/chat/views.py
@@ -8,12 +8,12 @@ def chat(request, chat_id):
     messages = LiveChatMessage.objects.filter(live_chat=chat).order_by("sent_at")
 
     # if the chat is public, add the request user to the chat
-    if chat.public and not chat.users.filter(id=request.user.id).exists():
+    if request.user.is_authenticated and chat.public and not chat.users.filter(id=request.user.id).exists():
         chat.users.add(request.user)
 
     return render(request, "chat/index.html", {"chat": chat, "messages": messages})
 
 
 def live_chat_list(request):
-    chats = LiveChat.objects.all()
+    chats = LiveChat.objects.filter(public=True)
     return render(request, "chat/live-chat-list.html", {"chats": chats})
