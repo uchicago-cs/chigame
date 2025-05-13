036b5c65ae0e5ecc37b96d895a6185a98d0ed16f
emilyzen <emilyzen17@gmail.com>
2025-05-13 16:28:14 -0500

    linter

--- src/chigame/achievements/urls.py
+++ src/chigame/achievements/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [path("achievements/demo-game", views.demo_game, name="demo-game")]
--- src/chigame/achievements/views.py
+++ src/chigame/achievements/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
+
 def demo_game(request):
     return render(request, "achievements/demo_game.html")
