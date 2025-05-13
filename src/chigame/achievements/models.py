088a904f85f44d8ba802e2501802d9d5269279aa
Ned Tagtmeier <ntagtmeier@uchicago.edu>
2025-05-13 15:42:30 -0500

    Revert "[achievements] updated UserAchievement time fields"
    This reverts commit f696a3d95cbcac2b299b72436f499d922df0f490.

--- src/chigame/achievements/models.py
+++ src/chigame/achievements/models.py
@@ -35,10 +35,9 @@ class UserAchievement(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
     pinned = models.BooleanField(default=False)
-    date_earned = models.DateTimeField(null=True, blank=True)
-    last_updated = models.DateTimeField(auto_now=True)
+    date_earned = models.DateTimeField()
     progress = models.FloatField(null=True, blank=True, default=1)
-    # progress can be updated if achievement has a threshold
+    # progress can be updated as user makes progress on an achievement with a threshold
 
     class Meta:
         unique_together = ("user", "achievement")
