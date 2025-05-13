1d53b24678c83c7eab63dea8fd1c628e306d33ae
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-05-12 22:46:44 -0500

    Add live chat list view and update navigation to include Live Chats link

--- src/chigame/chat/views.py
+++ src/chigame/chat/views.py
@@ -8,3 +8,7 @@ def chat(request, chat_id):
     messages = LiveChatMessage.objects.filter(live_chat=chat).order_by("sent_at")
 
     return render(request, "chat/index.html", {"chat": chat, "messages": messages})
+
+def live_chat_list(request):
+    chats = LiveChat.objects.all()
+    return render(request, "chat/live_chat_list.html", {"chats": chats})
