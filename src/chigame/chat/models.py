2c06bde77315efe85104bcc84934545fd2565cc8
Alexandros Lekkas <alexandros.lekkas@outlook.com>
2025-05-12 23:45:57 -0500

    [Live Chat] Add optional reply_to field to LiveChatMessage model

--- src/chigame/chat/models.py
+++ src/chigame/chat/models.py
@@ -24,6 +24,7 @@ class LiveChatMessage(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     sent_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField(null=False)
+    reply_to = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"Message: [{self.content}] by {self.user} in LiveChat {self.live_chat}"
