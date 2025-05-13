e9b1c0531e6a4d1fc2a63427c6e4cc2385add720
emilyzen <emilyzen17@gmail.com>
2025-05-13 16:25:05 -0500

    linter

--- src/chigame/achievements/urls.py
+++ src/chigame/achievements/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from . import views
 
-# Create your urls here
-
 urlpatterns = [path("achievements/demo-game", views.demo_game, name="demo-game")]
--- src/chigame/achievements/views.py
+++ src/chigame/achievements/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def demo_game(request):
     return render(request, "achievements/demo_game.html")
