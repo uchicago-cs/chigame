4d9f8706825fb447f26cc658835f9bd7870c8609
Md Hossain <hossainmd@Nerdy>
2025-05-12 21:38:50 -0500

    [users] added view support for notif sections #906

--- src/chigame/users/urls.py
+++ src/chigame/users/urls.py
@@ -53,4 +53,5 @@ urlpatterns = [
     path("notifications/<int:pk>/bookmark/", view=bookmark_notification, name="bookmark-notification"),
     path("notifications/<int:pk>/bookmarked/", view=view_bookmarked_notifications, name="bookmarked-notifications"),
     path("notifications/<int:pk>/move/", views.move_notification, name="move-notification"),
+    path("inbox/<int:pk>/<str:category>/", views.user_inbox_view, name="user-inbox-category"),
 ]
