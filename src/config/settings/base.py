664c4689da42246c466c5bfc910fb6dd53614476
zaynacheema <146792920+zaynacheema@users.noreply.github.com>
2025-05-14 18:11:18 -0500

    Update base.py with media settings for IF
    add media url and media root for IF importation

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -408,3 +408,9 @@ MACHINA_DEFAULT_AUTHENTICATED_USER_FORUM_PERMISSIONS = [
 # ------------------------------------------------------------------------------
 # https://channels.readthedocs.io/en/stable/topics/channel_layers.html
 CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}}
+
+#for interactive fiction
+import os
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
