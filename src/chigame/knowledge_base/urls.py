099e3054a438b6a0e8cd34f8f065de5537b87e63
Abril Rodriguez <abrilrodriguez@Abrils-MacBook-Air.local>
2025-05-14 20:46:22 -0500

    [API] modified knowledge base files to address CI test error in PR #984

--- src/chigame/knowledge_base/urls.py
+++ src/chigame/knowledge_base/urls.py
@@ -6,13 +6,13 @@ from .views import (
     DefaultView,
     DownloadGuide,
     FeedbackDetail,
-    GuideDetail,
+    GuideDetailView,
     ModeratorGuidesPending,
 )
 
 urlpatterns = [
     path("", DefaultView.as_view(), name="knowledge-base"),
-    path("guides/<int:pk>", GuideDetail.as_view(), name="knowledge-base-guide-detail"),
+    path("guides/<int:pk>", GuideDetailView.as_view(), name="knowledge-base-guide-detail"),
     path("moderation", ModeratorGuidesPending.as_view(), name="knowledge-base-moderator"),
     path("manage-my-guides", ContributorManageGuide.as_view(), name="contributor-manage-guide"),
     path("guides/<int:pk>/download", DownloadGuide, name="download-guide"),
