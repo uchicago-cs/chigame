2fa830ede9ce8ad648ec86afd42b7651d948b50d
Nathaniel Lee <nathaniellee@Nathaniels-MacBook-Pro.local>
2025-05-14 21:29:28 -0500

    added url for pinning message

--- src/chigame/chat/urls.py
+++ src/chigame/chat/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("<int:chat_id>/", views.chat, name="chat-page"),
     path("message/<int:message_id>/delete", views.delete_message, name="delete-message"),
+    path("message/<int:message_id>/pin", views.pin_message, name="pin-message"),
 ]
