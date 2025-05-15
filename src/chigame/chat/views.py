52b42506e89e2983ce15cdf9f0134ccaeb449e19
Nathaniel Lee <nathaniellee@Nathaniels-MacBook-Pro.local>
2025-05-14 21:08:23 -0500

    added pin_message view for pinned messages in chats

--- src/chigame/chat/views.py
+++ src/chigame/chat/views.py
@@ -33,3 +33,39 @@ def delete_message(request, message_id):
     message.delete()
 
     return JsonResponse({"message": "Message deleted successfully"}, status=200)
+
+
+def pin_message(request, message_id):
+    """
+    Pins a message in the chat
+
+    Args:
+        request: The request object.
+        message_id: The id of the message to delete.
+
+    Returns:
+        A JSON response.
+    """
+    if not request.user.is_authenticated:
+        return JsonResponse({"error": "Unauthorized"}, status=401)
+
+    message = get_object_or_404(LiveChatMessage, id=message_id)
+
+    if not message.live_chat.users.filter(id=request.user.id).exists():
+        return JsonResponse({"error": "Unauthorized"}, status=401)
+
+    chat = message.live_chat
+    chat.pinned_message = message
+    chat.save()
+
+    return JsonResponse(
+        {
+            "message": "Message pinned successfully",
+            "pinned_message": {
+                "id": message.id,
+                "content": message.content,
+                "username": message.user.username,
+            },
+        },
+        status=200,
+    )
