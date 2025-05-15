173c669cc6ff5f496f47ab2dcac8e4a7701e70af
Abril Rodriguez <abrilrodriguez@Abrils-MacBook-Air.local>
2025-05-14 22:15:32 -0500

    [API] addressed merge conflicts

--- src/chigame/api/serializers.py
+++ src/chigame/api/serializers.py
@@ -103,4 +103,19 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ["id", "user", "title", "rating", "review", "is_public", "created_at"]
+<<<<<<< HEAD
         read_only_fields = ["id", "created_at", "user"]
+=======
+
+
+class UserAchievementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAchievement
+        fields = ["id", "user", "pinned", "date_earned", "progress"]
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Achievement
+        fields = ["id", "name", "description", "rarity", "threshold"]
+>>>>>>> e941049f ([API] addressed merge conflicts)
