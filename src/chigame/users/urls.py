800ab172bdc7dfebe68e0e5fa52f704ea6f5dc44
jonathanbacon22 <jonathanbacon@uchicago.edu>
2025-05-15 12:25:14 -0500

    [User Profiles + Friending] Added urls for custom notifications - Issue #961

--- src/chigame/users/urls.py
+++ src/chigame/users/urls.py
@@ -57,4 +57,11 @@ urlpatterns = [
     path("notifications/<int:pk>/bookmarked/", view=view_bookmarked_notifications, name="bookmarked-notifications"),
     path("upload-photo/", upload_profile_photo, name="upload-photo"),
     path("notifications/<int:pk>/move/", views.move_notification, name="move-notification"),
+    path("labels/create/", views.create_notification_label, name="create-notification-label"),
+    path(
+        "notifications/<int:notification_id>/assign_label/",
+        views.assign_label_to_notification,
+        name="assign-notification-label",
+    ),
+    path("labels/<int:label_id>/notifications/", views.notifications_by_label, name="notifications-by-label"),
 ]
