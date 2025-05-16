9eb79b6b95b3116bcdb786c01ee3f3bc3e89db4f
jiayuew1 <121912893+jiayuew1@users.noreply.github.com>
2025-05-15 21:00:04 -0500

    Update urls.py

--- src/chigame/achievements/urls.py
+++ src/chigame/achievements/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
 from . import views
+
 urlpatterns = [
     path("demo-game", views.demo_game, name="demo-game"),
     path("", views.user_achievements, name="user_achievements"),
