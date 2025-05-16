7548861a32724913f8a93e2fd15b1865a8988a56
jiayuew1 <121912893+jiayuew1@users.noreply.github.com>
2025-05-16 16:03:13 -0500

    Update urls.py

--- src/chigame/achievements/urls.py
+++ src/chigame/achievements/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("demo-game", views.demo_game, name="demo-game"),
     path("", views.user_achievements, name="user_achievements"),
     path("<str:username>/", views.user_achievements, name="user_achievements_by_username"),
-    path("toggle-pin/<int:achievement_id>/", views.toggle_pin_achievement, name="toggle_pin_achievement"),
 ]
