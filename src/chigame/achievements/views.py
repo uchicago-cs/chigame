8b9cb9b14e36ea4e18944dbdb1fc9fa838da0385
Benjamin Sim <thatguyben@Benjamins-Laptop.local>
2025-05-18 13:26:28 -0500

    [achievements] implemented helper function to display 5 most recent achievements to be displayed on a user's profile page

--- src/chigame/achievements/views.py
+++ src/chigame/achievements/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from .models import UserAchievement
+
+
+def get_recent_achievements(pk, limit=5):
+    """
+    Helper function to get the most recent achievements for a user. This function should be used to call
+    the most recent achievements to be displayed on the user's profile page.
+    Args:
+        pl (int): The primary key of the user whose achievements are to be fetched.
+        limit (int): The maximum number of recent achievements to fetch. Default is 5.
+    """
+    return UserAchievement.objects.filter(user_id=pk).order_by("-date")[:limit]
