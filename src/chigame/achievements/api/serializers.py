45694d6d0925188feee2b2c05981a8783113fdba
emilyzen <emilyzen17@gmail.com>
2025-05-18 17:09:19 -0500

    fixing linter issues again

--- src/chigame/achievements/api/serializers.py
+++ src/chigame/achievements/api/serializers.py
@@ -3,10 +3,10 @@ from ..models import *
 
 class AchievementSerializer(serializers.ModelSerializer):
     class Meta:
-      model = Achievement
-      fields = ['name', 'description', 'spoiler', 'rarity', 'game', 'threshold']
+        model = Achievement
+        fields = ['name', 'description', 'spoiler', 'rarity', 'game', 'threshold']
 
 class UserAchievementSerializer(serializers.ModelSerializer):
     class Meta:
-      model = UserAchievement
-      fields = ['user', 'achievement', 'pinned', 'date_earned', 'progress']
+        model = UserAchievement
+        fields = ['user', 'achievement', 'pinned', 'date_earned', 'progress']
