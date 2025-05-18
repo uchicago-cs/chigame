b8ee0f7d25218107a4bf65e7ed159e96a7192414
jiayuew1 <121912893+jiayuew1@users.noreply.github.com>
2025-05-18 18:39:59 -0500

    Update views.py

--- src/chigame/achievements/views.py
+++ src/chigame/achievements/views.py
@@ -3,9 +3,6 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
-from chigame.games.models import Game
-from chigame.users.models import User
-
 from .models import Achievement, UserAchievement
 
 
