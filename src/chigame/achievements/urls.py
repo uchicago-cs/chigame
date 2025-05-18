5d58f1f8dfe8afcb91858988a2782f7515d138c2
jiayuew1 <121912893+jiayuew1@users.noreply.github.com>
2025-05-18 18:25:17 -0500

    Update urls.py

--- src/chigame/achievements/urls.py
+++ src/chigame/achievements/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("demo-game", views.demo_game, name="demo-game")]
+urlpatterns = [
+  path("demo-game", views.demo_game, name="demo-game"),
+  path("toggle-pin/<int:achievement_id>/", views.toggle_pin_achievement, name="toggle_pin_achievement"),
+]
