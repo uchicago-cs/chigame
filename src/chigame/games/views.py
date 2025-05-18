f70cfe90dd355d600709dda5761163d7ad42eddb
zaynacheema <146792920+zaynacheema@users.noreply.github.com>
2025-05-18 18:16:52 -0500

    Update GameCreatView for IF

--- src/chigame/games/views.py
+++ src/chigame/games/views.py
@@ -119,11 +119,9 @@ class GameCreateView(UserPassesTestMixin, CreateView):
         return context
 
     def get_success_url(self):
-        # If the game has a Twine .html file, go to IF view
-        if self.object.twine_file and self.object.twine_file.name.endswith(".html"):
-            return reverse("if-game", kwargs={"pk": self.object.pk})
-        # Otherwise, go to the normal game detail page
-        return reverse("game-detail", kwargs={"pk": self.object.pk})
+    if self.object.twine_file and self.object.twine_file.name.endswith(".html"):
+        return reverse("interactive-fiction-detail", kwargs={"pk": self.object.pk})
+    return reverse("game-detail", kwargs={"pk": self.object.pk})
 
 
 class GameEditView(UserPassesTestMixin, UpdateView):
