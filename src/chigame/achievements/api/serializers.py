3c19e72ec7e148420bd5ae8aca22167d8693bd37
emilyzen <emilyzen17@gmail.com>
2025-05-18 17:07:06 -0500

    fixing linter issues

--- src/chigame/achievements/api/serializers.py
+++ src/chigame/achievements/api/serializers.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 from ..models import *
 
 class AchievementSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Achievement
-    fields = ['name', 'description', 'spoiler', 'rarity', 'game', 'threshold']
+    class Meta:
+      model = Achievement
+      fields = ['name', 'description', 'spoiler', 'rarity', 'game', 'threshold']
 
 class UserAchievementSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = UserAchievement
-    fields = ['user', 'achievement', 'pinned', 'date_earned', 'progress']
+    class Meta:
+      model = UserAchievement
+      fields = ['user', 'achievement', 'pinned', 'date_earned', 'progress']
--- src/chigame/achievements/api/urls.py
+++ src/chigame/achievements/api/urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path("", views.get_achievements),
-    path("user-achievements/",views.get_user_achievements)
-]
+urlpatterns = [path("", views.get_achievements), path("user-achievements/",views.get_user_achievements)]
