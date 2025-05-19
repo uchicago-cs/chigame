04b944d07d871664a04c03bd05e9ae75a2ea703a
Alec Bargher <abargher@uchicago.edu>
2025-05-18 23:18:06 -0500

    Fix a missing comma in import after addressing merge conflict

--- src/chigame/games/views.py
+++ src/chigame/games/views.py
@@ -30,7 +30,7 @@ from chigame.users.models import User
 from .filters import LobbyFilter
 
 from .forms import GameForm, IFGameForm, LobbyForm, ReviewForm
-from .models import Chat, Checkers, CheckersTurn Game, GameList, InteractiveFictionGame, Lobby, Match, Player, Review, Tournament
+from .models import Chat, Checkers, CheckersTurn, Game, GameList, InteractiveFictionGame, Lobby, Match, Player, Review, Tournament
 from .simulation_utils import TournamentSimulator, run_complete_tournament_simulation
 from .tables import LobbyTable
 
