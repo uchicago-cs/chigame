79ad18916b36ab2ad7ba18c2a64a73e561970ce2
mrollinswaterman <mrollinswaterman@uchicago.edu>
2025-05-19 16:51:54 -0500

    [knowledge-base] Added frotend formatting and styling to feedback notifications on the Landing Page.

--- src/chigame/knowledge_base/views.py
+++ src/chigame/knowledge_base/views.py
@@ -276,9 +276,15 @@ class ReviewPendingGuideView(LoginRequiredMixin, UserPassesTestMixin, DetailView
         context["feedback"] = feedback
         context["message"] = message
 
-        msg = f"Guide reviewed successfully! {self.object.author.username}'s {self.object.game_id.name} Guide was {action}ed."
+        msg = f"""
+            Guide reviewed successfully!
+            {self.object.author.username}'s {self.object.game_id.name} Guide was {action}ed.
+            """
         if action == "request_changes":
-            msg = f"Guide reviewed successfully! You requested changes on {self.object.author.username}'s {self.object.game_id.name} Guide."
+            msg = f"""
+            Guide reviewed successfully!
+            You requested changes on {self.object.author.username}'s {self.object.game_id.name} Guide.
+            """
 
         messages.info(request, msg, extra_tags="guide-reviewed")
 
