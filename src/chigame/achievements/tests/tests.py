f0dfbdc4680b8da8b9369711a935efd10864cc22
Ned Tagtmeier <ntagtmeier@uchicago.edu>
2025-05-19 12:13:53 -0500

    [achievements] Moved method to achievement class + tests

--- src/chigame/achievements/tests/tests.py
+++ src/chigame/achievements/tests/tests.py
@@ -1,6 +1,8 @@
 import pytest
 
-from .factories import MatchFactory
+from chigame.achievements.models import Achievement
+
+from .factories import AchievementFactory, MatchFactory
 
 
 @pytest.mark.django_db
@@ -10,3 +12,10 @@ def test_game_users():
     assert len(match.game.users.all()) == len(match.players.all())
     for player in match.players.all():
         assert player in match.game.users.all()
+
+
+@pytest.mark.django_db
+def test_get_achievement():
+    achievement = AchievementFactory.create()
+    game = achievement.game
+    assert Achievement.get_achievement(name=achievement.name, game=game) == achievement
