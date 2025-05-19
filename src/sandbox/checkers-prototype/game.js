723bbaead737c646dfe848678222bbaf0ea00992
Alex Wang <alex.yp.wang@gmail.com>
2025-05-19 15:03:18 -0500

    style added to selected coord btn

--- src/sandbox/checkers-prototype/game.js
+++ src/sandbox/checkers-prototype/game.js
@@ -753,6 +753,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   toggleCoordinatesBtn.addEventListener('click', () => {
     coordsVisible = !coordsVisible;
+    toggleCoordinatesBtn.classList.add('selected');
 
     if (coordsVisible) {
       // get board position on screen
@@ -796,6 +797,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     } else {
       // remove coordinates
+      toggleCoordinatesBtn.classList.remove('selected');
       coordElements.forEach(el => document.body.removeChild(el));
       coordElements.length = 0;
     }
