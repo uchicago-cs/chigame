0486db07ad8abe52c6d53befa928c56ae000d90c
Enzo Nakornsri <ennako@uchicago.edu>
2025-05-19 16:31:24 -0500

    merge score

--- src/static/checkers-prototype/game.js
+++ src/static/checkers-prototype/game.js
@@ -56,7 +56,9 @@ async function fetchInitialBoardState() {
 }
 
 // ---INIT FUNCTIONS-----------------------------------------------------------
-function preload() { }
+function preload() {
+
+}
 
 async function create() {
   const state = await fetchInitialBoardState();
@@ -70,7 +72,9 @@ async function create() {
   }
 }
 
-function update() { }
+function update() {
+
+}
 // ----------------------------------------------------------------------------
 
 // Draw the game board
@@ -223,6 +227,8 @@ function isValidMove(piece, moveX, moveY) {
 }
 
 function movePiece(piece, moveX, moveY) {
+  updateScore();
+
   const dx = moveX - piece.x;
   const dy = moveY - piece.y;
 
@@ -310,6 +316,17 @@ function getBoardState() {
   return board;
 }
 
+// Updates score on frontend
+function updateScore() {
+  const score = document.getElementById('score');
+  const redOnBoard = pieces.filter(p => p.color === COLORS.red).length;
+  const blackOnBoard = pieces.filter(p => p.color === COLORS.black).length;
+  const redCaptured = 12 - redOnBoard;
+  const blackCaptured = 12 - blackOnBoard;
+  score.innerHTML = `Red: ${redCaptured}<br>Black: ${blackCaptured}`;
+}
+
+
 // If you have more than one tab open it will automatically update by calling
 // from the server
 
@@ -325,6 +342,7 @@ function reloadBoardFromState(state) {
 }
 
 function update() {
+  updateScore();
   // Every 2 seconds, poll server for board state
   if (!window.lastPollTime || Date.now() - window.lastPollTime > 2000) {
     window.lastPollTime = Date.now();
