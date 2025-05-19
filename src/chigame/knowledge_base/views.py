a541468ff02b1c684d85d1f1ef12203d2e58aabc
mrollinswaterman <146126429+mrollinswaterman@users.noreply.github.com>
2025-05-19 17:52:10 -0500

    [knowledge-base] Added redirect funtionality to review guide submission buttons (#1051)
    * [knowledge-base] Added redirect funtionality to review guide submission buttons
    
    * [knowledge-base] Removed UI tweaks because in light of PR #985, they are no longer needed.
    
    ---------
    
    Co-authored-by: David Makoyo <makoyo.david@me.com>

--- src/chigame/knowledge_base/views.py
+++ src/chigame/knowledge_base/views.py
@@ -275,7 +275,8 @@ class ReviewPendingGuideView(LoginRequiredMixin, UserPassesTestMixin, DetailView
         context = self.get_context_data(object=self.object)
         context["feedback"] = feedback
         context["message"] = message
-        return self.render_to_response(context)
+
+        return redirect("knowledge-base-moderator")
 
 
 class ModeratorListByGame(LoginRequiredMixin, UserPassesTestMixin, ListView):
