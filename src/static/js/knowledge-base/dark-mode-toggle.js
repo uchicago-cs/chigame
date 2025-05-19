e4f104af510d176cb0149f6129642634bb63c4d3
David Makoyo <makoyo.david@me.com>
2025-05-19 18:13:01 -0500

    Delete src/static/js/knowledge-base/dark-mode-toggle.js

--- src/static/js/knowledge-base/dark-mode-toggle.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const toggleButton = document.getElementById('dark-mode-toggle');
-  toggleButton.addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-  });
-});
