76cad1b6d34eb8484c93e6d2d7b5be22ed42a985
jiayuew1 <121912893+jiayuew1@users.noreply.github.com>
2025-05-19 21:53:30 -0500

    Update urls.py

--- src/chigame/achievements/urls.py
+++ src/chigame/achievements/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("demo-game", views.demo_game, name="demo-game"),
     path("", views.user_achievements, name="user_achievements"),
-    path('<int:user_id>/', views.user_achievements, name='user_achievements'),
+    path("<int:user_id>/", views.user_achievements, name='user_achievements'),
 ]
