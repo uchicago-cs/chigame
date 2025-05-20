154d025831687c546bb4f66d5dae18493b5bcfc8
Elizaveta Kabaeva <ekabaeva@uchicago.edu>
2025-05-20 16:34:41 -0500

    Implementation of a review form as a separate page with a redirection from the game-detail page

--- src/chigame/games/urls.py
+++ src/chigame/games/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path("bgg_search_by_name/", views.bgg_search_by_name, name="bgg_search_by_name"),
     path("search/", views.search_results, name="game-search-results"),
     path("<int:pk>/reviews/", views.ReviewListView.as_view(), name="game-review-list"),
+    path("games/<int:pk>/review/", views.add_review, name="add-review"),
     # interactive fiction
     path("interactive-fiction/", views.InteractiveFictionView.as_view(), name="interactive-fiction"),
     path("<int:pk>/upload/", UploadFileView.as_view(), name="upload-file"),
