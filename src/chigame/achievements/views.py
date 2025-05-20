f06eaba99d19736cc348f96602ffbd7ae6a05df7
jiayuew1 <121912893+jiayuew1@users.noreply.github.com>
2025-05-19 21:48:07 -0500

    Update views.py

--- src/chigame/achievements/views.py
+++ src/chigame/achievements/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 
 from chigame.games.models import Game
 from chigame.users.models import User
