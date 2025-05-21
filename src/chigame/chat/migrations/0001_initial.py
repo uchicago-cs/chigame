6729d0c954f57eca365d58896628170f0ce57300
Quincy Leung <quincyleung@uchicago.edu>
2025-05-21 16:59:56 -0500

    Fix lint and parenthesis issue

--- src/chigame/chat/migrations/0001_initial.py
+++ src/chigame/chat/migrations/0001_initial.py
@@ -46,6 +46,7 @@ class Migration(migrations.Migration):
                 ("user", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
+        migrations.CreateModel(
             name="LiveChatPoll",
             fields=[
                 ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
