7910929a678b597bb4f5467954a034a611cf4a4e
zaynacheema <146792920+zaynacheema@users.noreply.github.com>
2025-05-20 23:42:05 -0500

    [IF] fix string error from changing file field to binary field in models..

--- src/chigame/games/views.py
+++ src/chigame/games/views.py
@@ -564,7 +564,7 @@ class InteractiveFictionView(TemplateView):
         context["game"] = latest_game
 
         if latest_game.twine_file:
-            context["uploaded_file_url"] = latest_game.twine_file.url
+            context["has_uploaded_twine"] = bool(latest_game.twine_file)
 
         return context
 
