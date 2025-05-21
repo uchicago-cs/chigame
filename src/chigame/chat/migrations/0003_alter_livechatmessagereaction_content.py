8b278e362342d7a4b1bfe30775e333a16af73942
Nathaniel Lee <nathaniellee@Nathaniels-MacBook-Pro.local>
2025-05-21 14:10:21 -0500

    fixed migration errors

--- src/chigame/chat/migrations/0003_alter_livechatmessagereaction_content.py
+++ src/chigame/chat/migrations/0003_alter_livechatmessagereaction_content.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("chat", "0002_livechatmessagereaction"),
+        ("chat", "0002_livechatmessage_reply_to_livechatmessagereaction"),
     ]
 
     operations = [
