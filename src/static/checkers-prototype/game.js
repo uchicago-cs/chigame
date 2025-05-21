9e5ce5915d2515b0ce4d1b4ebfb886e26fa6e675
Enzo Nakornsri <ennako@uchicago.edu>
2025-05-20 21:30:05 -0500

    bug fixed

--- src/static/checkers-prototype/game.js
+++ src/static/checkers-prototype/game.js
@@ -67,7 +67,7 @@ async function fetchInitialBoardState() {
   }
 }
 
-// ---INIT FUNCTIONS-----------------------------------------------------------
+// ---INIT FUNCTIONS----------------------------------------------------------
 function preload() {
 
 }
