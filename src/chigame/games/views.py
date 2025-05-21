174db46210c6dd3788dca69b46ad1f5438647e17
zaynacheema <146792920+zaynacheema@users.noreply.github.com>
2025-05-20 23:19:27 -0500

    [IF] fix char versus binary field reading

--- src/chigame/games/views.py
+++ src/chigame/games/views.py
@@ -751,7 +751,7 @@ class IFGameCreateView(CreateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        latest_game = Game.objects.filter(twine_file__isnull=False).order_by("-id").first()
+        latest_game = Game.objects.filter(twine_file_content__isnull=False).order_by("-id").first()
 
         if not latest_game:
             # fallback dummy game to prevent pk=None
