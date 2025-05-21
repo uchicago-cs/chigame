fc9f9ecb5802791406b88f4e9e4ad65ef771b13c
David Ma <davidma.inspire@outlook.com>
2025-05-20 23:38:59 -0500

    [users] WPI: Removed FriendTable, created card template for friend list
    WPI: #886

--- src/chigame/users/models.py
+++ src/chigame/users/models.py
@@ -343,7 +343,7 @@ class Notification(models.Model):
             self.FRIEND_REQUEST: "friend",
             self.REMINDER: "system",
             self.UPCOMING_MATCH: "match",
-            self.MATCH_PROPOSAL: "match",
+            self.MATCH_INVITATION: "match",
             self.GROUP_INVITATION: "group",
             self.ACHIEVEMENT: "achievement",
         }
