f27fa6ce6446ebf0ca6708a0582c5030fb2cf7af
emilyzen <emilyzen17@gmail.com>
2025-05-21 12:29:00 -0500

    trying to fix tests failing

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -339,8 +339,6 @@ SOCIALACCOUNT_FORMS = {"signup": "chigame.users.forms.UserSocialSignupForm"}
 # Add additional configuration below:
 # ------------------------------------------------------------------------------
 
-current_module = sys.modules[__name__]
-
 # REST FRAMEWORK
 # Get the absolute project directory
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..'))
@@ -349,22 +347,22 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..
 achievements_api_dir = os.path.join(project_root, 'src', 'chigame', 'achievements', 'api')
 
 # Check if the directory exists
-if os.path.isdir(achievements_api_dir):
-    REST_FRAMEWORK = {
-        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-        "PAGE_SIZE": 10,
-        "DEFAULT_AUTHENTICATION_CLASSES": [
-            "rest_framework.authentication.SessionAuthentication",
-        ],
-    }
-else:
-    REST_FRAMEWORK = {
-        "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-        "PAGE_SIZE": 10,
-        "DEFAULT_AUTHENTICATION_CLASSES": [
-            "rest_framework_simplejwt.authentication.JWTAuthentication",
-        ],
-    }
+# if os.path.isdir(achievements_api_dir):
+#     REST_FRAMEWORK = {
+#         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+#         "PAGE_SIZE": 10,
+#         "DEFAULT_AUTHENTICATION_CLASSES": [
+#             "rest_framework.authentication.SessionAuthentication",
+#         ],
+#     }
+# else:
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
 
 CORS_ALLOWED_ORIGINS = [
     # Vue frontend URL
