58368ba2e6ad2dedfbf708a856c9bc0bad6d305c
Nathaniel Lee <nathaniellee@Nathaniels-MacBook-Pro.local>
2025-05-21 16:42:27 -0500

    fix erros

--- src/chigame/chat/forms.py
+++ src/chigame/chat/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+
 from .models import LiveChat
+
 
 class LiveChatForm(forms.ModelForm):
     class Meta:
-        model = LiveChat    
+        model = LiveChat
         fields = ["name", "description", "public"]
-        
