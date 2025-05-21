6a9f4c347700a7843ec82a24171295d9f5923630
emilyzen <emilyzen17@gmail.com>
2025-05-21 12:58:41 -0500

    linter

--- src/chigame/achievements/api/views.py
+++ src/chigame/achievements/api/views.py
@@ -56,15 +56,11 @@ def award_achievement(request):
                 max_players=1,
                 complexity=1,
             )[0]
-            achievement = Achievement.objects.get_or_create(
-                name = "Clicked a Button",
-                rarity = 1,
-                game = game
-            )[0]
+            achievement = Achievement.objects.get_or_create(name = "Clicked a Button", rarity = 1, game = game)[0]
             user = request.user
             user_achievement = UserAchievement.objects.get_or_create(
                 user=user, achievement=achievement, date_earned="2025-04-24T21:45:37.084000Z"
             )
 
-            response_data = {'message': 'Button press received'}
+            response_data = {"message": "Button press received"}
             return JsonResponse(response_data)
