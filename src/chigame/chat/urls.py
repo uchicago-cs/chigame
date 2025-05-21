331a9e01df40ade89d819cebe3f7f255c398c28e
Nathaniel Lee <nathaniellee@Nathaniels-MacBook-Pro.local>
2025-05-21 16:49:27 -0500

    added views

--- src/chigame/chat/urls.py
+++ src/chigame/chat/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("<int:chat_id>/", views.chat, name="chat-page"),
     path("live-chat-list/", views.live_chat_list, name="live-chat-list"),
     path("live-chat-list/create/", views.create_live_chat, name="create-live-chat"),
+    path("live-chat-list/leave/<int:chat_id>/", views.leave_chat, name="leave-chat"),
     path("message/<int:message_id>/delete", views.delete_message, name="delete-message"),
     path("message/<int:message_id>/react", views.react_to_message, name="react-to-message"),
 ]
