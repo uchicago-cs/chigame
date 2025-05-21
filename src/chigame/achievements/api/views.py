02bc85d921eba28f958024ed2b277ae42fceb956
emilyzen <emilyzen17@gmail.com>
2025-05-21 13:06:21 -0500

    what is going on

--- src/chigame/achievements/api/views.py
+++ src/chigame/achievements/api/views.py
@@ -56,7 +56,7 @@ def award_achievement(request):
                 max_players=1,
                 complexity=1,
             )[0]
-            achievement = Achievement.objects.get_or_create(name = "Clicked a Button", rarity = 1, game = game)[0]
+            achievement = Achievement.objects.get_or_create(name="Clicked a Button", rarity=1, game=game)[0]
             user = request.user
             user_achievement = UserAchievement.objects.get_or_create(
                 user=user, achievement=achievement, date_earned="2025-04-24T21:45:37.084000Z"
