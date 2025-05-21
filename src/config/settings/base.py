e2b65d7f44bef7a99b9e4848fb51c689c78527ba
emilyzen <emilyzen17@gmail.com>
2025-05-21 12:34:22 -0500

    trying things

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -347,7 +347,7 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..
 achievements_api_dir = os.path.join(project_root, 'src', 'chigame', 'achievements', 'api')
 
 # Check if the directory exists
-# if os.path.isdir(achievements_api_dir):
+#
 #     REST_FRAMEWORK = {
 #         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
 #         "PAGE_SIZE": 10,
@@ -356,13 +356,14 @@ achievements_api_dir = os.path.join(project_root, 'src', 'chigame', 'achievement
 #         ],
 #     }
 # else:
-REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ],
-}
+if not os.path.isdir(achievements_api_dir):
+  REST_FRAMEWORK = {
+      "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+      "PAGE_SIZE": 10,
+      "DEFAULT_AUTHENTICATION_CLASSES": [
+          "rest_framework_simplejwt.authentication.JWTAuthentication",
+      ],
+  }
 
 CORS_ALLOWED_ORIGINS = [
     # Vue frontend URL
