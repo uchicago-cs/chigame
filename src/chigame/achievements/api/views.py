39c6044283e9a94871c162435f512f2f02da1d38
emilyzen <emilyzen17@gmail.com>
2025-05-21 12:56:15 -0500

    linter again

--- src/chigame/achievements/api/views.py
+++ src/chigame/achievements/api/views.py
@@ -50,11 +50,11 @@ def award_achievement(request):
     if request.method == "POST":
         if request.user.is_authenticated:
             game = Game.objects.get_or_create(
-                name = "Demo Game",
-                description = "Game for demonstrating achievements.",
-                min_players = 1,
-                max_players = 1,
-                complexity = 1,
+                name="Demo Game",
+                description="Game for demonstrating achievements.",
+                min_players=1,
+                max_players=1,
+                complexity=1,
             )[0]
             achievement = Achievement.objects.get_or_create(
                 name = "Clicked a Button",
@@ -63,7 +63,7 @@ def award_achievement(request):
             )[0]
             user = request.user
             user_achievement = UserAchievement.objects.get_or_create(
-                user = user, achievement = achievement, date_earned = "2025-04-24T21:45:37.084000Z"
+                user=user, achievement=achievement, date_earned="2025-04-24T21:45:37.084000Z"
             )
 
             response_data = {'message': 'Button press received'}
