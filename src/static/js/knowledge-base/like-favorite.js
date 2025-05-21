54515cade694ab14d712258f564f136c03d568f4
Yi Zou <yizou@Yis-MacBook-Air.local>
2025-05-21 15:46:57 -0500

    [knowledge-base] Modify like/fav button text
    - modify the text so that when it's already liked/favorited, the text
    displays "unlike"/"unfavorite"
    
    For: #910

--- src/static/js/knowledge-base/like-favorite.js
+++ src/static/js/knowledge-base/like-favorite.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const likeIcon = document.getElementById("like-icon");
     const likeurl = likeSpan.getAttribute('data-like-url');
     const likeCount = document.getElementById('like-count');
+    const likeText = document.getElementById('like-text');
 
     likeSpan.addEventListener("click", function () {
         fetch(likeurl, {
@@ -28,9 +29,11 @@ document.addEventListener("DOMContentLoaded", function () {
             if (data.liked) {
                 likeIcon.classList.remove("heart-hollow");
                 likeIcon.classList.add("heart-liked");
+                likeText.textContent = `Unlike`;
               } else {
                 likeIcon.classList.remove("heart-liked");
                 likeIcon.classList.add("heart-hollow");
+                likeText.textContent = `Like`;
               }
             likeCount.textContent = `${data.like_count} ♥`;
         })
@@ -42,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const favIcon = document.getElementById("fav-icon");
     const favurl = favSpan.getAttribute('data-fav-url');
     const favCount = document.getElementById('fav-count');
+    const favText = document.getElementById('fav-text');
 
     favSpan.addEventListener("click", function () {
         fetch(favurl, {
@@ -63,9 +67,11 @@ document.addEventListener("DOMContentLoaded", function () {
             if (data.favorited) {
                 favIcon.classList.remove("star-hollow");
                 favIcon.classList.add("star-faved");
+                favText.textContent = `Unfavorite`;
               } else {
                 favIcon.classList.remove("star-faved");
                 favIcon.classList.add("star-hollow");
+                favText.textContent = `Favorite`;
               }
             favCount.textContent = `${data.fav_count} ★`;
         })
