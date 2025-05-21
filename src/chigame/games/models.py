ecfa75e00d0dbd4c44a0be60fbfa8ac993a07cc5
zaynacheema <146792920+zaynacheema@users.noreply.github.com>
2025-05-20 19:34:01 -0500

    [IF] add twine file name and content for database storage

--- src/chigame/games/models.py
+++ src/chigame/games/models.py
@@ -39,6 +39,8 @@ class Game(models.Model):
     game_url = models.URLField(blank=True, null=True, help_text="URL for embedded games (e.g., external web games)")
     # interactive fiction  - twine file
     twine_file = models.FileField(upload_to="twine_games/", null=True, blank=True)
+    twine_file_name = models.CharField(max_length=255, null=True, blank=True)
+    twine_file_content = models.BinaryField(null=True, blank=True)
 
     suggested_age = models.PositiveSmallIntegerField(
         null=True, blank=True
