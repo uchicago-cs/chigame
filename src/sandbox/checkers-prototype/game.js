fab3bc55a1be01a8787677416be875c583a3b8c6
Nathaniel <nneidlinger@uchicago.edu>
2025-05-21 21:39:18 -0500

    highlighted pieces resizing is fixed

--- src/sandbox/checkers-prototype/game.js
+++ src/sandbox/checkers-prototype/game.js
@@ -436,40 +436,6 @@ function endTurn(scene) {
   clearHighlightedTiles();
 }
 
-// helper function to highlight the valid moves for the selected piece
-function highlightValidMoves(scene, piece) {
-  clearHighlightedTiles(); // remove any previous highlights
-
-  // iterate through the board
-  for (let y = 0; y < BOARD_SIZE; y++) {
-    for (let x = 0; x < BOARD_SIZE; x++) {
-      // check that the tile is not occupied and is a valid move
-      if (!getPiece(x, y) && isValidMove(piece, x, y)) {
-        // add a slighlty transparent white square on top of that tile to make
-        // the tile appear highlighted
-        const highlight = scene.add.rectangle(
-          margin + x * tile_size + tile_size / 2,
-          margin + y * tile_size + tile_size / 2,
-          tile_size,
-          tile_size,
-          0xffffff,
-          0.3
-        );
-        // add the game object to the array (so we can keep track and delete later)
-        highlightedTiles.push(highlight);
-      }
-    }
-  }
-}
-
-// helper function to clear all the highlighted tiles
-function clearHighlightedTiles() {
-  // remove each rect from the screen and then pop the reference from the array
-  while (highlightedTiles.length > 0) {
-    highlightedTiles.pop().destroy();
-  }
-}
-
 // generate a random valid move
 // after we finish implementing a bot, we could it make give an actual good suggestion
 function giveHint() {
@@ -547,70 +513,6 @@ function clearHighlightedTiles() {
   // remove each rect from the screen and then pop the reference from the array
   while (highlightedTiles.length > 0) {
     highlightedTiles.pop().destroy();
-  }
-}
-
-// generate a random valid move
-// after we finish implementing a bot, we could it make give an actual good suggestion
-function giveHint() {
-  // get all of the pieces of the current player
-  const playerPieces = pieces.filter((p) => p.color === currentPlayer);
-  let validMoves = [];
-
-  // get all of the valid moves for all of the pieces
-  for (const piece of playerPieces) {
-    for (let y = 0; y < BOARD_SIZE; y++) {
-      for (let x = 0; x < BOARD_SIZE; x++) {
-        if (!getPiece(x, y) && isValidMove(piece, x, y)) {
-          // add it to the array
-          // (this is a shorthand to initialize objects btw if you don't know)
-          validMoves.push({ piece, x, y });
-        }
-      }
-    }
-  }
-
-  // We have yet to implement a feature where it checks whether there are valid
-  // moves remaining for a player after each turn. In a real game of checkers
-  // if there are no moves left, the player loses the game.
-  if (validMoves.length > 0) {
-    // Math.random() only returns floating point from 0 to 1 and would require a
-    // separate helper function to return a random index from the array...
-    // https://docs.phaser.io/phaser/concepts/math
-    // phaser.math.rnd.pick() selects a random element from the array
-    const randomHint = Phaser.Math.RND.pick(validMoves);
-    // unlike chess, where there's rank and file, I don't think there's a proper
-    // way of calling a specific square on the board. For now, I just have it return
-    // the row and col on the matrix.
-    alert(
-      `Hint: Move ${currentPlayer === COLORS.red ? 'red' : 'black'} piece at (row ${
-        randomHint.piece.y
-      }, column ${randomHint.piece.x}) to (row ${randomHint.y}, column ${randomHint.x})`
-    );
-  } else {
-    // in a normal checkers game, the player loses if there are no moves left
-    alert('No valid moves.');
-  }
-
-
-  // reset draw offer if it was made by the current player
-  if (drawOffered && drawOfferedBy === currentPlayer) {
-    const gameOverPrompts = document.getElementById('gameOverPrompts');
-    const gameOverMessage = document.getElementById('gameOverMessage');
-    const drawBtn = document.getElementById('drawBtn');
-    const declineDrawBtn = document.getElementById('declineDrawBtn');
-    drawOffered = false;
-    drawOfferedBy = null;
-    gameOverMessage.textContent = '';
-    gameOverMessage.classList.remove('show');
-    gameOverPrompts.classList.remove('show');
-    drawBtn.textContent = 'Offer Draw';
-    declineDrawBtn.style.display = 'none';
-  }
-  //if black and bot is on, schedule bot move
-  if (vsEasyBot && currentPlayer === COLORS.black){
-    //delay so user has time to process bot movw after their own
-    scene.time.delayedCall(300, easyBot, [scene], scene);
   }
 }
 
@@ -860,6 +762,7 @@ function resizegame(percentage) {
     if (selectedPiece === piece) {
       selectedPiece.sprite = piece.sprite;
       piece.sprite.setStrokeStyle(highlight_size, COLORS.white);
+      highlightValidMoves(checkers.scene.scenes[0], piece);
     }
 
     // make the new sprite do stuff when clicked
@@ -881,6 +784,7 @@ function resizegame(percentage) {
         // highlight the current piece that is being selected and set them as 'selectedPiece'
         selectedPiece = piece;
         piece.sprite.setStrokeStyle(highlight_size, COLORS.white);
+        highlightValidMoves(checkers.scene.scenes[0], piece);
       }
     });
   });
