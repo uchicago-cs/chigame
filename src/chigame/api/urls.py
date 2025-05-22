0efd1fa1389b721c348a0a3ecc4cd716d0a48b5f
Abril Rodriguez Almanzar <155594542+abrodriguezalman@users.noreply.github.com>
2025-05-22 13:47:07 -0500

    [API] Finished implementation of USER ACHIEVEMENT Detail View Endpoint (#1339)
    * [API] Finished implementation of USER_ACHIEVEMENT detail view API endpoint
    
    * Fixed mistake with user_patterns url in urls.py, comments not supported
    
    * [API] Made adjustments to fix CI errors
    
    ---------
    
    Co-authored-by: Abril Rodriguez <abrilrodriguez@Abrils-MacBook-Air.local>
    Co-authored-by: theworldofobi <112725097+theworldofobi@users.noreply.github.com>
    Co-authored-by: Sam Xie <72187977+MuyanXie@users.noreply.github.com>

--- src/chigame/api/urls.py
+++ src/chigame/api/urls.py
@@ -35,6 +35,11 @@ user_patterns = [
     path("<slug:slug>/", views.UserDetailView.as_view(), name="api-user-detail"),
     path("<slug:slug>/groups/", views.UserGroupsView.as_view(), name="api-user-groups"),
     path("<int:pk>/friends/", views.UserFriendsAPIView.as_view(), name="api-user-friends"),
+    path(
+        "<int:user_id>/achievements/<int:pk>",
+        views.UserAchievementDetailView.as_view(),
+        name="api-user-achievements-edit",
+    ),
     path("<int:pk>/achievements/", views.UserAchievementListView.as_view(), name="api-user-achievements"),
 ]
 
