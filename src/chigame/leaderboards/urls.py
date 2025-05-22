27cb6d69054ed78ebb69ef82f566958623ab3f7c
Thuc Dzu <thucdzu@Thucs-MacBook-Air.local>
2025-05-22 02:39:16 -0500

    fixed linter check failures

--- src/chigame/leaderboards/urls.py
+++ src/chigame/leaderboards/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import leaderboard_view
 from . import views
+from .views import leaderboard_view
 
 urlpatterns = [
     path("<int:game_id>/", views.leaderboard_view, name="leaderboard_view"),
-    path("bar-chart/<int:game_id>/", views.bar_chart, name="bar_chart")
+    path("bar-chart/<int:game_id>/", views.bar_chart, name="bar_chart"),
 ]
