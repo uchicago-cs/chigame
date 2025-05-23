3c01eee5e5957d406e114c1bac0f602a97f1abd1
Zayna Cheema <zaynacheema@MacBook-Pro-4.local>
2025-05-23 02:23:34 -0500

    Add fake migration to satisfy missing forum_conversation.0010 dependency

---
+++ src/chigame/forum_conversation/migrations/0010_auto_20170120_0224.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('forum_conversation', '0009_auto_20170119_1833'),  # or whatever the last real one was
+    ]
+
+    operations = []
