90072ef0b4f426cdb07d2140e6c98d03efad2276
Nathaniel Lee <nathaniellee@Nathaniels-MacBook-Pro.local>
2025-05-23 00:56:09 -0500

    added urls for profanity toggle

--- src/chigame/chat/urls.py
+++ src/chigame/chat/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("message/<int:message_id>/pin", views.pin_message, name="pin-message"),
     path("message/<int:message_id>/edit", views.edit_message, name="edit-message"),
     path("message/<int:message_id>/react", views.react_to_message, name="react-to-message"),
+    path("toggle-profanity/", views.toggle_profanity, name="toggle-profanity"),
 ]
