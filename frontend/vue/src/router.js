a43a2a3c5b47829841fbcf537f2edd8d4501c1c1
Rolando Vazquez <rtvazquez@uchicago.edu>
2025-05-22 21:38:58 -0500

    [frontend] created lobbies page, currently no real lobbies to link, but looks good as holder #1335

--- frontend/vue/src/router.js
+++ frontend/vue/src/router.js
@@ -6,6 +6,7 @@ import ForumsPage from "./pages/ForumsPage.vue";
 import SignupPage from "./pages/SignupPage.vue";
 import ProfilePage from "./pages/ProfilePage.vue";
 import GameLibrary from "./pages/GameLibrary.vue";
+import LobbiesPage from "./pages/LobbiesPage.vue";
 
 const routes = [
   { path: '/', component: HomePage },
@@ -18,6 +19,8 @@ const routes = [
   { path: '/signup', component: SignupPage },
   { path: '/profile', component: ProfilePage },
   { path: '/games', component: GameLibrary },
+  { path: '/lobbies', component: LobbiesPage },
+
 ];
 
 const router = createRouter({
