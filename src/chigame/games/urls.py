6e443c3880bae08ef5427b52aa9090fe734561e5
carrieluo05 <148005605+carriesunluo05@users.noreply.github.com>
2025-05-22 21:15:50 -0500

    [Tournament] Testing for the Tournament Feedback System (#1070)
    * [Tournaments] updated frontend service
    
    * [Tournaments] updated frontend service
    
    * [Tournament] Added automated tests for Feedback
    
    ---------
    
    Co-authored-by: Sam Xie <72187977+MuyanXie@users.noreply.github.com>

--- src/chigame/games/urls.py
+++ src/chigame/games/urls.py
@@ -56,6 +56,12 @@ urlpatterns = [
     path("feedback/my-feedback/", views.user_feedback_list, name="user-feedback-list"),
     # Word Game
     path("wordle/", views.wordle_game_page, name="wordle-game"),
+    # tournament feedback
+    path("tournaments/<int:tournament_id>/feedback/", views.tournament_feedback_list, name="tournament-feedback-list"),
+    path("tournaments/<int:tournament_id>/feedback/submit/", views.submit_feedback, name="submit-feedback"),
+    path("feedback/update/<int:feedback_id>/", views.update_feedback_view, name="update-feedback"),
+    path("feedback/delete/<int:feedback_id>/", views.delete_feedback_view, name="delete-feedback"),
+    path("feedback/my-feedback/", views.user_feedback_list, name="user-feedback-list"),
     # checkers
     path("checkers/<int:pk>/", views.checkers_game_view, name="checkers-game"),
     path("checkers/<int:board_id>/update/", views.checkers_game_update_board_state, name="update_board_state"),
