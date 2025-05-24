a4da1df254b629ded335187b4b4a80955321b482
harriet-fisher <harrietfisher@uchicago.edu>
2025-05-24 17:56:09 -0500

    Fixed merging dev

--- src/chigame/users/views.py
+++ src/chigame/users/views.py
@@ -546,9 +546,7 @@ def friend_list_view(request, pk):
     target_user = get_object_or_404(User, pk=pk)
     # fetch the target user's friends
     friends = target_user.friends.all()
-    # render the friends table (keeping for backward compatibility)
-    table = FriendsTable(friends)
-    context = {"table": table, "friends": friends}
+    context = {"friends": friends}
 
     # if the target user is the current user, render the friends list
     if pk == user.id:
