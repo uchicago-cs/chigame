8e26d793cdc9742058a3077295955c43c7969be9
Abril Rodriguez <abrilrodriguez@Abrils-MacBook-Air.local>
2025-05-23 23:01:01 -0500

    [API] Modified files to address CI test error

--- src/chigame/api/tests/factories.py
+++ src/chigame/api/tests/factories.py
@@ -6,7 +6,6 @@ from factory import Faker, Iterator, LazyAttribute, LazyFunction, Sequence, SubF
 from factory.django import DjangoModelFactory
 
 from chigame.achievements.models import Achievement, UserAchievement
-from chigame.achievements.models import Achievement
 from chigame.chat.models import LiveChat, LiveChatUser
 from chigame.games.models import Category, Chat, Feedback, Game, Lobby, Match, Mechanic, Review, Tournament
 from chigame.users.models import User
@@ -198,6 +197,7 @@ class AchievementFactory(DjangoModelFactory):
     threshold = Faker("pydecimal", left_digits=1, right_digits=1, min_value=1)
     game = SubFactory(GameFactory)
 
+
 class UserAchievementFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = UserAchievement
@@ -208,6 +208,7 @@ class UserAchievementFactory(factory.django.DjangoModelFactory):
     progress = Faker("pydecimal", left_digits=1, right_digits=1, min_value=1)
     date_earned = Faker("date_time_this_year")
     last_updated = Faker("date_time_this_year")
+
 
 class LiveChatFactory(factory.django.DjangoModelFactory):
     class Meta:
