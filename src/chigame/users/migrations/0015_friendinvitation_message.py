b62f022efbd6a8e623433d45c8d7e81d6a51908a
harriet-fisher <harrietfisher@uchicago.edu>
2025-05-26 18:50:06 -0500

    fixed migration issues

--- src/chigame/users/migrations/0015_friendinvitation_message.py
+++ src/chigame/users/migrations/0015_friendinvitation_message.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("users", "0014_alter_friendinvitation_unique_together"),
+        ("users", "0013_recommendationpreferences"),
     ]
 
     operations = [
