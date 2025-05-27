79f280de9a615dd1151a0c1821e60780af1713a1
Grace Harrell <Grace@Graces-MacBook-Pro-2.local>
2025-05-27 17:13:13 -0500

    [IF] Update django comment and imports

--- src/chigame/games/views.py
+++ src/chigame/games/views.py
@@ -1,4 +1,5 @@
 import json
+import os
 import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
 from functools import wraps
@@ -11,6 +12,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
+from django.core.files.storage import FileSystemStorage
 from django.core.paginator import Paginator
 from django.db.models import Avg, Case, Count, ExpressionWrapper, F, FloatField, Q, Value, When
 from django.db.models.functions import Lower
@@ -21,7 +23,7 @@ from django.urls import reverse, reverse_lazy
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.utils.timezone import now
-from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView, View
 from django.views.generic.edit import FormMixin
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -757,6 +759,7 @@ class GameDeleteView(DeleteView):
             return HttpResponseForbidden("You don't have permission to delete this game.")
         return super().dispatch(request, *args, **kwargs)
 
+
 class UploadFileView(View):
     def post(self, request, pk=None):
         uploaded_file = request.FILES.get("uploaded_file")
@@ -787,7 +790,8 @@ class UploadFileView(View):
 
         messages.error(request, "No file selected.")
         return redirect("game-list")
-      
+
+
 # =============== Tournaments Views ===============
 
 
